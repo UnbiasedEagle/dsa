3867048268f66ce95fc7198e00c7c3dd2201861e
Saurabh Singh <imsaurabhsingh007@gmail.com>
2024-12-06 19:55:52 +0530

    solve Single Number

--- index.js
+++ index.js
@@ -1,37 +1,15 @@
 class Solution {
   /**
-   * @param {number[][]} grid
+   * @param {number[]} nums
    * @return {number}
    */
+  singleNumber(nums) {
+    let result = 0;
 
-  dfs(grid, i, j) {
-    if (
-      i < 0 ||
-      j < 0 ||
-      i >= grid.length ||
-      j >= grid[0].length ||
-      grid[i][j] == 0
-    )
-      return 0;
-    grid[i][j] = 0;
-    return (
-      1 +
-      this.dfs(grid, i + 1, j) +
-      this.dfs(grid, i - 1, j) +
-      this.dfs(grid, i, j + 1) +
-      this.dfs(grid, i, j - 1)
-    );
-  }
-
-  maxAreaOfIsland(grid) {
-    let maxArea = 0;
-    for (let i = 0; i < grid.length; i++) {
-      for (let j = 0; j < grid[0].length; j++) {
-        if (grid[i][j] == 1) {
-          maxArea = Math.max(maxArea, this.dfs(grid, i, j));
-        }
-      }
+    for (let i = 0; i < nums.length; i++) {
+      result ^= nums[i];
     }
-    return maxArea;
+
+    return result;
   }
 }
