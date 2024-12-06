0276f2a3e1abb8bb8d936bf081389e0a3d4ae707
Saurabh Singh <imsaurabhsingh007@gmail.com>
2024-12-06 15:22:04 +0530

    solve Max Area of Island

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
 class Solution {
   /**
-   * @param {character[][]} grid
+   * @param {number[][]} grid
    * @return {number}
    */
+
   dfs(grid, i, j) {
     if (
       i < 0 ||
       j < 0 ||
       i >= grid.length ||
-      j >= grid[i].length ||
-      grid[i][j] !== '1'
-    ) {
-      return;
-    }
-    grid[i][j] = '0';
-    this.dfs(grid, i + 1, j);
-    this.dfs(grid, i - 1, j);
-    this.dfs(grid, i, j + 1);
-    this.dfs(grid, i, j - 1);
+      j >= grid[0].length ||
+      grid[i][j] == 0
+    )
+      return 0;
+    grid[i][j] = 0;
+    return (
+      1 +
+      this.dfs(grid, i + 1, j) +
+      this.dfs(grid, i - 1, j) +
+      this.dfs(grid, i, j + 1) +
+      this.dfs(grid, i, j - 1)
+    );
   }
 
-  numIslands(grid) {
-    let count = 0;
+  maxAreaOfIsland(grid) {
+    let maxArea = 0;
     for (let i = 0; i < grid.length; i++) {
-      for (let j = 0; j < grid[i].length; j++) {
-        if (grid[i][j] === '1') {
-          count++;
-          this.dfs(grid, i, j);
+      for (let j = 0; j < grid[0].length; j++) {
+        if (grid[i][j] == 1) {
+          maxArea = Math.max(maxArea, this.dfs(grid, i, j));
         }
       }
     }
-    return count;
+    return maxArea;
   }
 }
