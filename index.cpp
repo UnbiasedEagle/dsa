0885ea1a61ef316b63fe5b0194e0d0f994e842e7
Saurabh Singh <imsaurabhsingh007@gmail.com>
2024-12-14 21:12:37 +0530

    Solve Even Number of Digits

--- index.cpp
+++ index.cpp
@@ -1,10 +1,25 @@
-int getIdenticalTwinsCount(vector<int> &arr) {
-    unordered_map<int, int>map;
-    int result=0;
+bool isEven(int num) {
+    int digits=0;
 
-    for(int i = 0;i<arr.size();i++){
-        result+=map[arr[i]];
-        map[arr[i]]++;
+    if(num==0){
+        return false;
+    }
+
+    while(num>0){
+        digits++;
+        num/=10;
+    }
+
+    return digits%2==0;
+}
+
+vector<int> getEvenDigitNumbers(vector<int> &arr) {
+    vector<int> result;
+
+    for(int i=0;i<arr.size();i++){
+        if(isEven(arr[i])){
+            result.push_back(arr[i]);
+        }
     }
 
     return result;
