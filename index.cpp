c5d2d9f9a56d596d6219732b6cb562e4439207d8
Saurabh Singh <imsaurabhsingh007@gmail.com>
2024-12-14 20:35:19 +0530

    Solve Positive Cumulative Sum

--- index.cpp
+++ index.cpp
@@ -1,9 +1,23 @@
-vector<int> getCumulativeSum(vector<int> &arr) {
-    for(int i = 1;i<arr.size();i++){
-        arr[i] = arr[i-1] + arr[i];
+vector<int> getPositiveCumulativeSum(vector<int> &arr) {
+	// add your logic here
+    vector<int>ans;
+
+    int sum = arr[0];
+
+    if(sum>0){
+        ans.push_back(sum);
     }
-    return arr;
+
+    for(int i=1;i<arr.size();i++){
+        sum += arr[i];
+
+        if(sum>0){
+            ans.push_back(sum);
+        }
+    }
+
+    return ans;
 }
 
-// Time Complexity: O(n)
-// Space Complexity: O(1)
+// Time Complexity: O(N)
+// Space Complexity: O(N)
