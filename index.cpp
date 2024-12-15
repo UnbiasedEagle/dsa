3002827f9990c0761cfe7c5387a0682d31eebf74
Saurabh Singh <imsaurabhsingh007@gmail.com>
2024-12-15 16:59:29 +0530

    Solve Implement Merge Sort

--- index.cpp
+++ index.cpp
@@ -1,34 +1,44 @@
-void merge(vector<int> &arr, int endIndex) {
-    // add your logic here
-    vector<int>result;
-
-    int i = 0;
-    int j = endIndex + 1;
-
-    while(i <= endIndex && j < arr.size()) {
-        if(arr[i] < arr[j]) {
-            result.push_back(arr[i]);
+void merge(vector<int> &arr, int l, int m, int r) {
+    vector<int>temp;
+    int i=l;
+    int j=m+1;
+    while(i<=m && j<=r){
+        if(arr[i]<=arr[j]){
+            temp.push_back(arr[i]);
             i++;
-        } else {
-            result.push_back(arr[j]);
+        }
+        else{
+            temp.push_back(arr[j]);
             j++;
         }
     }
-
-    while(i <= endIndex) {
-        result.push_back(arr[i]);
+    while(i<=m){
+        temp.push_back(arr[i]);
         i++;
     }
-
-    while(j < arr.size()) {
-        result.push_back(arr[j]);
+    while(j<=r){
+        temp.push_back(arr[j]);
         j++;
     }
-
-    for(int i = 0; i < arr.size(); i++) {
-        arr[i] = result[i];
+    for(int i=l;i<=r;i++){
+        arr[i]=temp[i-l];
     }
 }
 
-// Time Complexity: O(n)
+void helper(vector<int> &arr, int l, int r) {
+    if(l>=r){
+        return;
+    }
+    int m = l + (r-l)/2;
+    helper(arr, l, m);
+    helper(arr, m+1, r);
+    merge(arr, l, m, r);
+}
+
+void mergeSort(vector<int> &arr) {
+    // add your logic here
+    helper(arr, 0, arr.size()-1);
+}
+
+// Time Complexity: O(nlogn)
 // Space Complexity: O(n)
