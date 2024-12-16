b7757c25f6aa10dced4bfb100043267ab4e4f0f7
Saurabh Singh <imsaurabhsingh007@gmail.com>
2024-12-16 19:31:16 +0530

    solve Square sorted array

--- index.cpp
+++ index.cpp
@@ -1,32 +1,12 @@
-int partition(vector<int> &arr, int low, int high) {
-    int pivot = arr[high];
-    int i = low;
-    for (int j = low; j < high; j++) {
-        if (arr[j] < pivot) {
-            swap(arr[i], arr[j]);
-            i++;
-        }
-    }
-    swap(arr[i], arr[high]);
-    return i;
-}
-
-void helper(vector<int> &arr, int low, int high) {
-    if(low>=high){
-        return;
-    }
-
-    int pivot = partition(arr, low, high);
-    helper(arr, low, pivot-1);
-    helper(arr, pivot+1, high);
-}
-
-
-void quickSort(vector<int> &arr) {
+vector<int> getSquareSortedArray(vector<int> &arr) {
     // add your logic here
-    helper(arr, 0, arr.size()-1);
+    for(int i=0;i<arr.size();i++){
+        arr[i]=arr[i]*arr[i];
+    }
+
+    sort(arr.begin(),arr.end());
+    return arr;
 }
 
-// Average Time Complexity: O(NlogN)
-// Worst Time Complexity: O(N^2)
-// Space Complexity: O(logN)
+// Time Complexity: O(NlogN)
+// Space Complexity: O(1)
