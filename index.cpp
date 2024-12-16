0191c74bfb35be1a904e6b4eafc86a8189bf5c0b
Saurabh Singh <imsaurabhsingh007@gmail.com>
2024-12-16 21:01:29 +0530

    Solve Largest Contiguous Sum

--- index.cpp
+++ index.cpp
@@ -1,21 +1,14 @@
-bool isArithmeticSequence(vector<int> &arr) {
-    // add your logic here
-    if(arr.size() == 1){
-        return true;
+int largestContiguousSum(vector <int> &arr){
+    int maxSum = arr[0];
+    int currSum = arr[0];
+
+    for(int i = 1; i < arr.size(); i++){
+        currSum = max(arr[i], currSum + arr[i]);
+        maxSum = max(maxSum, currSum);
     }
 
-    sort(arr.begin(), arr.end());
-
-    int diff = arr[1] - arr[0];
-
-    for(int i = 2; i < arr.size(); i++){
-        if(arr[i] - arr[i-1] != diff){
-            return false;
-        }
-    }
-
-    return true;
+    return maxSum;
 }
 
-// Time Complexity: O(nlogn)
+// Time Complexity: O(n)
 // Space Complexity: O(1)
