5c9d6131b606e0a86e62a9383e9adf91afdfcc38
Saurabh Singh <imsaurabhsingh007@gmail.com>
2024-12-17 10:09:25 +0530

    Solve Row Column Zero

--- index.cpp
+++ index.cpp
@@ -1,14 +1,46 @@
-vector<int> pascalTriangleRow(int rowNo) {
-    rowNo--;
-    vector<int> row;
-    row.push_back(1);
-    int C = 1; // Binomial coefficient
-    for (int i = 1; i <= rowNo; i++) {
-        C = C * (rowNo-i+1)/i;
-        row.push_back(C);
+void setRowColumnZeroes(vector<vector<int> > &matrix) {
+    // add your logic here
+    int row = matrix.size();
+    int col = matrix[0].size();
+
+    bool firstRowZero = false;
+    bool firstColZero = false;
+
+    for(int i = 0; i < row; i++){
+        if(matrix[i][0] == 0){
+            firstColZero = true;
+        }
     }
-    return row;
+
+    for(int i = 0; i < col; i++){
+        if(matrix[0][i] == 0){
+            firstRowZero = true;
+        }
+    }
+
+    for(int i = row-1; i > 0; i--){
+        for(int j = col-1; j > 0; j--){
+            if(matrix[i][j] == 0){
+                matrix[i][0] = 0;
+                matrix[0][j] = 0;
+            }
+        }
+    }
+
+    for(int i = row-1; i >= 0; i--){
+        for(int j = col-1; j >= 0; j--){
+           if(i!=0 && j!=0){
+               if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                   matrix[i][j] = 0;
+               }
+           }else if(i==0 && firstRowZero){
+               matrix[i][j] = 0;
+           }else if(j==0 && firstColZero){
+               matrix[i][j] = 0;
+           }
+        }
+    }
 }
 
-// Time Complexity: O(n)
-// Space Complexity: O(n)
+// Time Complexity: O(m*n)
+// Space Complexity: O(1)
