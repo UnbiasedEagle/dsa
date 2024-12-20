d52a3735e951d0774907d93b1dc5ceb571ccc445
Saurabh Singh <imsaurabhsingh007@gmail.com>
2024-12-20 12:33:14 +0530

    Solve Search Range

--- index.cpp
+++ index.cpp
@@ -1,12 +1,36 @@
-bool containsElement(vector<int> &arr, int key) {
-    // add your logic here
-    for(int i=0;i<arr.size();i++){
-        if(arr[i]==key){
-            return true;
+int binarySearch(const vector<int> &arr, int key, bool findFirst) {
+    int low = 0;
+    int high = arr.size() - 1;
+    int result = -1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (arr[mid] == key) {
+            result = mid;
+            if (findFirst) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        } else if (arr[mid] > key) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
         }
     }
-    return false;
+    return result;
 }
 
-// Time Complexity: O(n)
-// Space Complexity: O(1)
+vector<int> searchRange(vector<int> &arr, int key) {
+    vector<int> ans;
+
+    int firstIdx = binarySearch(arr, key, true);
+    int lastIdx = binarySearch(arr, key, false);
+
+    ans.push_back(firstIdx);
+    ans.push_back(lastIdx);
+    return ans;
+}
+
+// Time complexity: O(log n)
+// Space complexity: O(1)
