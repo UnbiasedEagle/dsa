7a15b8ff928cd7c8fbb67ae6343f29faf93b8d83
Saurabh Singh <imsaurabhsingh007@gmail.com>
2024-12-20 11:24:06 +0530

    Solve Contains Element?

--- index.cpp
+++ index.cpp
@@ -1,17 +1,12 @@
-vector<vector<int> > mergeIntervals(vector<vector<int> > &intervals) {
+bool containsElement(vector<int> &arr, int key) {
     // add your logic here
-    sort(intervals.begin(),intervals.end());
-    vector<vector<int> > merged;
-    for(int i=0;i<intervals.size();i++){
-        if(merged.size()==0 || merged.back()[1]<intervals[i][0]){
-            merged.push_back(intervals[i]);
-        }
-        else{
-            merged.back()[1]=max(merged.back()[1],intervals[i][1]);
+    for(int i=0;i<arr.size();i++){
+        if(arr[i]==key){
+            return true;
         }
     }
-    return merged;
+    return false;
 }
 
-// Time Complexity: O(nlogn)
-// Auxilary Space Complexity: O(1)
+// Time Complexity: O(n)
+// Space Complexity: O(1)
