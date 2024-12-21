c9933fc1cbd10ea2af73e973bdd4f72fced6a131
Saurabh Singh <imsaurabhsingh007@gmail.com>
2024-12-21 19:37:57 +0530

    Solve Negative numbers in sorted array

--- index.cpp
+++ index.cpp
@@ -1,36 +1,20 @@
-int binarySearch(const vector<int> &arr, int key, bool findFirst) {
+int getNegativeNumbersCount(vector<int> &arr) {
+    // add your logic here
     int low = 0;
     int high = arr.size() - 1;
-    int result = -1;
-
-    while (low <= high) {
-        int mid = (low + high) / 2;
-        if (arr[mid] == key) {
-            result = mid;
-            if (findFirst) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-        } else if (arr[mid] > key) {
-            high = mid - 1;
-        } else {
+    int idx = -1;
+    while(low<=high){
+        int mid = low + (high-low)/2;
+        if(arr[mid] < 0){
+            idx = mid;
             low = mid + 1;
         }
+        else{
+            high = mid - 1;
+        }
     }
-    return result;
+    return idx + 1;
 }
 
-vector<int> searchRange(vector<int> &arr, int key) {
-    vector<int> ans;
-
-    int firstIdx = binarySearch(arr, key, true);
-    int lastIdx = binarySearch(arr, key, false);
-
-    ans.push_back(firstIdx);
-    ans.push_back(lastIdx);
-    return ans;
-}
-
-// Time complexity: O(log n)
-// Space complexity: O(1)
+// Time Complexity: O(log n)
+// Space Complexity: O(1)
