12792d138b5b87d00b89ea2095343cbf9f1b91fe
Saurabh Singh <imsaurabhsingh007@gmail.com>
2024-12-25 21:13:47 +0530

    Solve Kth Largest Element

--- index.cpp
+++ index.cpp
@@ -1,29 +1,50 @@
-/* This is the ListNode class definition
+#include <vector>
+#include <queue>
+using namespace std;
 
-class ListNode {
-public:
-    int data;
-    ListNode* next;
-
-    ListNode(int data) {
-        this->data = data;
-        this->next = NULL;
-    }
-};
-*/
-
-ListNode *kthElement(ListNode *head, int k)
+int partition(vector<int> &list, int left, int right)
 {
-    // add your logic here
-    ListNode *temp = head;
-    while (k > 1)
+    int pivot = list[right];
+    int i = left;
+    for (int j = left; j < right; j++)
     {
-        temp = temp->next;
-        k--;
+        if (list[j] < pivot)
+        {
+            swap(list[i], list[j]);
+            i++;
+        }
     }
-
-    return temp;
+    swap(list[i], list[right]);
+    return i;
 }
 
-// Time Complexity: O(N)
-// Space Complexity: O(1)
+int quickSelect(vector<int> &list, int left, int right, int k)
+{
+    if (left == right)
+    {
+        return list[left];
+    }
+
+    int pivotIndex = partition(list, left, right);
+
+    if (k == pivotIndex)
+    {
+        return list[k];
+    }
+    else if (k < pivotIndex)
+    {
+        return quickSelect(list, left, pivotIndex - 1, k);
+    }
+    else
+    {
+        return quickSelect(list, pivotIndex + 1, right, k);
+    }
+}
+
+int getKthLargestElement(vector<int> &list, int k)
+{
+    return quickSelect(list, 0, list.size() - 1, list.size() - k);
+}
+
+// Time Complexity: O(n)
+// Auxilary Space Complexity: O(log n)
