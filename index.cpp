047a22e320f443b32cccf851ccf3bf9fa33431bd
Saurabh Singh <imsaurabhsingh007@gmail.com>
2024-12-26 09:30:05 +0530

    Solve Sorted Arrays Intersection

--- index.cpp
+++ index.cpp
@@ -1,39 +1,34 @@
 #include <vector>
 using namespace std;
 
-vector<vector<int>> threeSum(vector<int> &A)
+vector<int> intersection(vector<int> &A, vector<int> &B)
 {
     // add your logic here
-    vector<vector<int>> res;
-    sort(A.begin(), A.end());
+    int i = 0;
+    int j = 0;
 
-    for (int i = 0; i < A.size(); i++)
+    vector<int> ans;
+
+    while (i < A.size() && j < B.size())
     {
-        if (i > 0 && A[i] == A[i - 1])
-            continue;
-        int l = i + 1, r = A.size() - 1;
-        while (l < r)
+        if (A[i] == B[j])
         {
-            int sum = A[i] + A[l] + A[r];
-            if (sum == 0)
-            {
-                res.push_back({A[i], A[l], A[r]});
-                while (l < r && A[l] == A[l + 1])
-                    l++;
-                while (l < r && A[r] == A[r - 1])
-                    r--;
-                l++;
-                r--;
-            }
-            else if (sum < 0)
-                l++;
-            else
-                r--;
+            ans.push_back(A[i]);
+            i++;
+            j++;
+        }
+        else if (A[i] < B[j])
+        {
+            i++;
+        }
+        else
+        {
+            j++;
         }
     }
 
-    return res;
+    return ans;
 }
 
-// Time Complexity: O(N^2)
+// Time Complexity: O(N + M)
 // Space Complexity: O(1)
