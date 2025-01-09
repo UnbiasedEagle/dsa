740039842ec9b3bb0cbfe55a19bca4415702b2a2
Saurabh Singh <imsaurabhsingh007@gmail.com>
2025-01-09 20:40:52 +0530

    implement selection sort

--- index.cpp
+++ index.cpp
@@ -1,16 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define int long long
 #define mod 1000000007
 #define inf 1e18
 #define endl "\n"
 
-void print1ToN(int n)
+void selectionSort(vector<int> &v)
 {
-    if (n == 0)
-        return;
-    print1ToN(n - 1);
-    cout << n << " ";
+    int n = v.size();
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (v[j] < v[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+
+        swap(v[i], v[minIndex]);
+    }
 }
 
 int32_t main()
@@ -23,7 +33,20 @@ int32_t main()
     freopen("output.txt", "w", stdout);
 #endif
 
-    print1ToN(5);
+    vector<int> v;
+
+    v.push_back(5);
+    v.push_back(4);
+    v.push_back(3);
+    v.push_back(2);
+    v.push_back(1);
+
+    selectionSort(v);
+
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
 
     return 0;
 }
