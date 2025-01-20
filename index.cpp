5ebdae46e588856c9eaea00ed2c9cdcd594ac670
Saurabh Singh <imsaurabhsingh007@gmail.com>
2025-01-20 20:49:50 +0530

    Solve 48. Rotate Image

--- index.cpp
+++ index.cpp
@@ -4,62 +4,36 @@ using namespace std;
 #define inf 1e18
 #define endl "\n"
 
-vector<vector<int>> zeroMatrix(vector<vector<int>> &matrix, int n, int m)
+class Solution
 {
-    bool isFirstColZero = false;
-    bool isFirstRowZero = false;
-
-    for (int i = 0; i < n; i++)
+public:
+    void rotate(vector<vector<int>> &matrix)
     {
-        if (matrix[i][0] == 0)
+        for (int i = 0; i < matrix.size(); i++)
         {
-            isFirstColZero = true;
-            break;
-        }
-    }
+            int j = matrix.size() - 1;
 
-    for (int i = 0; i < m; i++)
-    {
-        if (matrix[0][i] == 0)
-        {
-            isFirstRowZero = true;
-            break;
-        }
-    }
-
-    for (int i = n - 1; i >= 1; i--)
-    {
-        for (int j = m - 1; j >= 1; j--)
-        {
-            if (matrix[i][j] == 0)
+            while (j >= i)
             {
-                matrix[i][0] = 0;
-                matrix[0][j] = 0;
+                swap(matrix[i][j], matrix[j][i]);
+                j--;
+            }
+        }
+
+        for (int i = 0; i < matrix.size(); i++)
+        {
+            int start = 0;
+            int end = matrix.size() - 1;
+
+            while (start < end)
+            {
+                swap(matrix[i][start], matrix[i][end]);
+                start++;
+                end--;
             }
         }
     }
-
-    for (int i = n - 1; i >= 0; i--)
-    {
-        for (int j = m - 1; j >= 0; j--)
-        {
-            if (j == 0 && isFirstColZero)
-            {
-                matrix[i][j] = 0;
-            }
-            if (i == 0 && isFirstRowZero)
-            {
-                matrix[i][j] = 0;
-            }
-            else if (matrix[i][0] == 0 || matrix[0][j] == 0)
-            {
-                matrix[i][j] = 0;
-            }
-        }
-    }
-
-    return matrix;
-}
+};
 
 int32_t main()
 {
