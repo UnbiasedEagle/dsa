102a5154ac42b17f6358dfbbffcf86c3748bb8f4
Saurabh Singh <imsaurabhsingh007@gmail.com>
2025-01-23 15:57:12 +0530

    implement Longest Subarray With Zero Sum

--- index.cpp
+++ index.cpp
@@ -4,61 +4,28 @@ using namespace std;
 #define inf 1e18
 #define endl "\n"
 
-vector<vector<int>> fourSum(vector<int> &nums, int target)
+int getLongestZeroSumSubarrayLength(vector<int> &arr)
 {
-    vector<vector<int>> ans;
-    sort(nums.begin(), nums.end());
-    int n = nums.size();
+    int largest = 0;
+    int sum = 0;
+    unordered_map<int, int> mp;
+    mp[0] = -1;
 
-    int i = 0;
-    while (i < n)
+    for (int i = 0; i < arr.size(); i++)
     {
-        int j = i + 1;
-        while (j < n)
+        sum += arr[i];
+        if (mp.find(sum) != mp.end())
         {
-            int start = j + 1;
-            int end = n - 1;
-            while (start < end)
-            {
-                int sum = nums[i] + nums[j] + nums[start] + nums[end];
-                if (sum == target)
-                {
-                    ans.push_back({nums[i], nums[j], nums[start], nums[end]});
-                    while (start < end && nums[start] == nums[start + 1])
-                    {
-                        start++;
-                    }
-                    while (start < end && nums[end] == nums[end - 1])
-                    {
-                        end--;
-                    }
-                    start++;
-                    end--;
-                }
-                else if (sum > target)
-                {
-                    end--;
-                }
-                else
-                {
-                    start++;
-                }
-            }
-            j++;
-
-            while (j < n && nums[j] == nums[j - 1])
-            {
-                j++;
-            }
+            largest = max(largest, i - mp[sum]);
         }
-        i++;
-        while (i < n && nums[i] == nums[i - 1])
+
+        if (mp.find(sum) == mp.end())
         {
-            i++;
+            mp[sum] = i;
         }
     }
 
-    return ans;
+    return largest;
 }
 
 int32_t main()
@@ -70,21 +37,6 @@ int32_t main()
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
-
-    int n;
-    cin >> n;
-    vector<int> arr(n);
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
-
-    vector<vector<int>> ans = triplet(n, arr);
-
-    for (auto i : ans)
-    {
-        cout << i[0] << " " << i[1] << " " << i[2] << endl;
-    }
 
     return 0;
 }
