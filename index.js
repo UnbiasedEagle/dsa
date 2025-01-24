82790240271530f94a6f711ef9faeeada904eed3
Saurabh Singh <imsaurabhsingh007@gmail.com>
2025-01-24 20:15:00 +0530

    solve Container With Most Water

--- index.js
+++ index.js
@@ -1,42 +1,25 @@
 class Solution {
   /**
-   * @param {number[]} nums
-   * @return {number[][]}
+   * @param {number[]} heights
+   * @return {number}
    */
-  threeSum(nums) {
-    nums.sort((a, b) => a - b);
-    const result = [];
-    let i = 0;
+  maxArea(heights) {
+    let max = 0;
+    let left = 0;
+    let right = heights.length - 1;
 
-    while (i < nums.length) {
-      let left = i + 1;
-      let right = nums.length - 1;
-
-      while (left < right) {
-        const sum = nums[i] + nums[left] + nums[right];
-        if (sum === 0) {
-          result.push([nums[i], nums[left], nums[right]]);
-          left++;
-          right--;
-
-          while (left < right && nums[left] === nums[left - 1]) {
-            left++;
-          }
-          while (left < right && nums[right] === nums[right + 1]) {
-            right--;
-          }
-        } else if (sum < 0) {
-          left++;
-        } else {
-          right--;
-        }
-      }
-      i++;
-      while (i < nums.length && nums[i] === nums[i - 1]) {
-        i++;
+    while (left < right) {
+      const width = right - left;
+      const height = Math.min(heights[left], heights[right]);
+      const area = width * height;
+      max = Math.max(max, area);
+      if (heights[left] < heights[right]) {
+        left++;
+      } else {
+        right--;
       }
     }
 
-    return result;
+    return max;
   }
 }
