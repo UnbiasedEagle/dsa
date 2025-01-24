8616c7d9d0a09c91afea9bfea1d8a4eeb2a904b7
Saurabh Singh <imsaurabhsingh007@gmail.com>
2025-01-24 21:00:32 +0530

    solve Best Time to Buy and Sell Stock

--- index.js
+++ index.js
@@ -1,36 +1,20 @@
 class Solution {
   /**
-   * @param {number[]} height
+   * @param {number[]} prices
    * @return {number}
    */
-  trap(height) {
-    const maxLeftHeight = Array.from({ length: height.length }, () => 0);
-    const maxRightHeight = Array.from({ length: height.length }, () => 0);
+  maxProfit(prices) {
+    let buyPrice = prices[0];
+    let maxProfit = 0;
 
-    let leftMaxHeight = height[0];
-
-    for (let i = 0; i < height.length; i++) {
-      maxLeftHeight[i] = leftMaxHeight;
-      leftMaxHeight = Math.max(leftMaxHeight, height[i]);
-    }
-
-    let rightMaxHeight = height[height.length - 1];
-
-    for (let i = height.length - 1; i >= 0; i--) {
-      maxRightHeight[i] = rightMaxHeight;
-      rightMaxHeight = Math.max(rightMaxHeight, height[i]);
-    }
-
-    let totalWater = 0;
-
-    for (let i = 0; i < height.length; i++) {
-      const currentWater =
-        Math.min(maxLeftHeight[i], maxRightHeight[i]) - height[i];
-      if (currentWater > 0) {
-        totalWater += currentWater;
+    for (let i = 1; i < prices.length; i++) {
+      if (prices[i] < buyPrice) {
+        buyPrice = prices[i];
+      } else {
+        maxProfit = Math.max(maxProfit, prices[i] - buyPrice);
       }
     }
 
-    return totalWater;
+    return maxProfit;
   }
 }
