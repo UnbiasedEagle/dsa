0a77ea6bedc0bde1246baced50051e29afafb796
Saurabh Singh <imsaurabhsingh007@gmail.com>
2025-01-24 08:01:14 +0530

    solve Group Anagrams

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 class Solution {
   /**
-   * @param {number[]} nums
-   * @param {number} target
-   * @return {number[]}
+   * @param {string[]} strs
+   * @return {string[][]}
    */
-  twoSum(nums, target) {
+  groupAnagrams(strs) {
+    const result = [];
     const map = new Map();
-    for (let i = 0; i < nums.length; i++) {
-      const complement = target - nums[i];
-      if (map.has(complement)) {
-        return [map.get(complement), i];
+
+    for (let i = 0; i < strs.length; i++) {
+      const str = strs[i];
+      const freq = Array.from({ length: 26 }, () => 0);
+
+      for (let j = 0; j < str.length; j++) {
+        freq[str[j].charCodeAt(0) - 97]++;
       }
-      map.set(nums[i], i);
+
+      const key = freq.join('#');
+      if (map.has(key)) {
+        map.get(key).push(str);
+      } else {
+        map.set(key, [str]);
+      }
     }
+
+    Array.from(map.values()).forEach((v) => {
+      result.push(v);
+    });
+
+    return result;
   }
 }
