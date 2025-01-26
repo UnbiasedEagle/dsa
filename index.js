41c58f996d398699ef24c784f0a87deb1fd7f950
Saurabh Singh <imsaurabhsingh007@gmail.com>
2025-01-26 08:39:50 +0530

    solve Minimum Window Substring

--- index.js
+++ index.js
@@ -1,30 +1,64 @@
 class Solution {
   /**
-   * @param {string} s1
-   * @param {string} s2
-   * @return {boolean}
+   * @param {string} s
+   * @param {string} t
+   * @return {string}
    */
-  checkInclusion(s1, s2) {
-    if (s2.length < s1.length) {
-      return false;
+
+  isEqual(freq1, freq2) {
+    for (const [char, count] of freq1.entries()) {
+      if (!freq2.has(char) || freq2.get(char) < count) {
+        return false;
+      }
     }
-    const freq = Array.from({ length: 26 }, () => 0);
-    for (let i = 0; i < s1.length; i++) {
-      freq[s1.charCodeAt(i) - 'a'.charCodeAt(0)]--;
+    return true;
+  }
+
+  minWindow(s, t) {
+    if (t.length > s.length) {
+      return '';
+    }
+    const freq1 = new Map();
+    const freq2 = new Map();
+
+    // Build frequency map for target string
+    for (const char of t) {
+      freq1.set(char, (freq1.get(char) || 0) + 1);
     }
 
-    for (let i = 0; i < s2.length; i++) {
-      if (i < s1.length) {
-        freq[s2.charCodeAt(i) - 'a'.charCodeAt(0)]++;
-      } else {
-        freq[s2.charCodeAt(i) - 'a'.charCodeAt(0)]++;
-        freq[s2.charCodeAt(i - s1.length) - 'a'.charCodeAt(0)]--;
+    let left = 0;
+    let right = 0;
+    let startIndex = -1;
+    let minLength = Infinity;
+
+    while (right < s.length) {
+      // Add character to window frequency map
+      const rightChar = s[right];
+      freq2.set(rightChar, (freq2.get(rightChar) || 0) + 1);
+
+      while (left <= right && this.isEqual(freq1, freq2)) {
+        const currentLength = right - left + 1;
+        if (currentLength < minLength) {
+          minLength = currentLength;
+          startIndex = left;
+        }
+        // Remove character from window frequency map
+        const leftChar = s[left];
+        freq2.set(leftChar, freq2.get(leftChar) - 1);
+        if (freq2.get(leftChar) === 0) {
+          freq2.delete(leftChar);
+        }
+        left++;
       }
-      if (freq.every((count) => count === 0)) {
-        return true;
-      }
+
+      right++;
     }
-    return false;
+
+    if (startIndex === -1) {
+      return '';
+    }
+
+    return s.substring(startIndex, startIndex + minLength);
   }
 }
 
