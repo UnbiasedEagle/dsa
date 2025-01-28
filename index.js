d5d1ff9167e2dd733aedfdb153afcdfbfd7daed1
Saurabh Singh <imsaurabhsingh007@gmail.com>
2025-01-28 07:59:33 +0530

    Solve Car Fleet

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 class Solution {
   /**
-   * @param {number[]} temperatures
-   * @return {number[]}
+   * @param {number} target
+   * @param {number[]} position
+   * @param {number[]} speed
+   * @return {number}
    */
-  dailyTemperatures(temperatures) {
-    const stack = [];
-    const result = Array.from({ length: temperatures.length }, () => 0);
-
-    for (let i = 0; i < temperatures.length; i++) {
-      while (
-        stack.length &&
-        temperatures[stack[stack.length - 1]] < temperatures[i]
-      ) {
-        const index = stack.pop();
-        result[index] = i - index;
-      }
-      stack.push(i);
+  carFleet(target, position, speed) {
+    const arr = [];
+    for (let i = 0; i < position.length; i++) {
+      arr.push({
+        position: position[i],
+        time: (target - position[i]) / speed[i],
+      });
     }
-    return result;
+    arr.sort((a, b) => b.position - a.position);
+    let fleet = 1;
+    let maxTime = arr[0].time;
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i].time > maxTime) {
+        fleet++;
+        maxTime = arr[i].time;
+      }
+    }
+    return fleet;
   }
 }
 
-// Time complexity: O(n)
+// Time complexity: O(nlogn)
 // Space complexity: O(n)
