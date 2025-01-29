e91feebf95aa01e24d06137cf6a3dd31cce004b4
Saurabh Singh <imsaurabhsingh007@gmail.com>
2025-01-29 14:12:19 +0530

    Solve Median of Two Sorted Arrays

--- index.js
+++ index.js
@@ -1,41 +1,56 @@
-class TimeMap {
-  constructor() {
-    this.keyStore = new Map();
-  }
-
+class Solution {
   /**
-   * @param {string} key
-   * @param {string} value
-   * @param {number} timestamp
-   * @return {void}
+   * @param {number[]} nums1
+   * @param {number[]} nums2
+   * @return {number}
    */
-  set(key, value, timestamp) {
-    if (!this.keyStore.has(key)) {
-      this.keyStore.set(key, []);
-    }
-    this.keyStore.get(key).push([value, timestamp]);
-  }
+  findMedianSortedArrays(nums1, nums2) {
+    if (nums1.length > nums2.length)
+      return this.findMedianSortedArrays(nums2, nums1);
 
-  /**
-   * @param {string} key
-   * @param {number} timestamp
-   * @return {string}
-   */
-  get(key, timestamp) {
-    const values = this.keyStore.get(key) || [];
+    const totalElements = nums1.length + nums2.length;
+    const leftLength = Math.floor((totalElements + 1) / 2);
+
     let left = 0;
-    let right = values.length - 1;
-    let result = '';
+    let right = nums1.length;
+
     while (left <= right) {
-      const mid = Math.floor((left + right) / 2);
-      const [value, time] = values[mid];
-      if (time <= timestamp) {
-        result = value;
-        left = mid + 1;
+      const elementToTakeFromLeft = Math.floor((left + right) / 2);
+      const elementToTakeFromRight = leftLength - elementToTakeFromLeft;
+
+      const nums1Left =
+        elementToTakeFromLeft <= 0
+          ? -Infinity
+          : nums1[elementToTakeFromLeft - 1];
+      const nums1Right =
+        elementToTakeFromLeft >= nums1.length
+          ? Infinity
+          : nums1[elementToTakeFromLeft];
+      const nums2Left =
+        elementToTakeFromRight <= 0
+          ? -Infinity
+          : nums2[elementToTakeFromRight - 1];
+      const nums2Right =
+        elementToTakeFromRight >= nums2.length
+          ? Infinity
+          : nums2[elementToTakeFromRight];
+
+      if (nums1Left <= nums2Right && nums2Left <= nums1Right) {
+        if (totalElements % 2 === 0) {
+          return (
+            (Math.max(nums1Left, nums2Left) +
+              Math.min(nums1Right, nums2Right)) /
+            2
+          );
+        } else {
+          return Math.max(nums1Left, nums2Left);
+        }
+      } else if (nums1Left > nums2Right) {
+        right = elementToTakeFromLeft - 1;
       } else {
-        right = mid - 1;
+        left = elementToTakeFromLeft + 1;
       }
     }
-    return result;
+    return 0;
   }
 }
