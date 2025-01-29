ddaae2ac41cccc0382f3ea401ab5d0aa6a80c847
Saurabh Singh <imsaurabhsingh007@gmail.com>
2025-01-29 15:46:34 +0530

    Solve Reverse Linked List

--- index.js
+++ index.js
@@ -1,56 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
 class Solution {
   /**
-   * @param {number[]} nums1
-   * @param {number[]} nums2
-   * @return {number}
+   * @param {ListNode} head
+   * @return {ListNode}
    */
-  findMedianSortedArrays(nums1, nums2) {
-    if (nums1.length > nums2.length)
-      return this.findMedianSortedArrays(nums2, nums1);
+  reverseList(head) {
+    let prev = null;
+    let current = head;
 
-    const totalElements = nums1.length + nums2.length;
-    const leftLength = Math.floor((totalElements + 1) / 2);
-
-    let left = 0;
-    let right = nums1.length;
-
-    while (left <= right) {
-      const elementToTakeFromLeft = Math.floor((left + right) / 2);
-      const elementToTakeFromRight = leftLength - elementToTakeFromLeft;
-
-      const nums1Left =
-        elementToTakeFromLeft <= 0
-          ? -Infinity
-          : nums1[elementToTakeFromLeft - 1];
-      const nums1Right =
-        elementToTakeFromLeft >= nums1.length
-          ? Infinity
-          : nums1[elementToTakeFromLeft];
-      const nums2Left =
-        elementToTakeFromRight <= 0
-          ? -Infinity
-          : nums2[elementToTakeFromRight - 1];
-      const nums2Right =
-        elementToTakeFromRight >= nums2.length
-          ? Infinity
-          : nums2[elementToTakeFromRight];
-
-      if (nums1Left <= nums2Right && nums2Left <= nums1Right) {
-        if (totalElements % 2 === 0) {
-          return (
-            (Math.max(nums1Left, nums2Left) +
-              Math.min(nums1Right, nums2Right)) /
-            2
-          );
-        } else {
-          return Math.max(nums1Left, nums2Left);
-        }
-      } else if (nums1Left > nums2Right) {
-        right = elementToTakeFromLeft - 1;
-      } else {
-        left = elementToTakeFromLeft + 1;
-      }
+    while (current) {
+      const next = current.next;
+      current.next = prev;
+      prev = current;
+      current = next;
     }
-    return 0;
+
+    return prev;
   }
 }
