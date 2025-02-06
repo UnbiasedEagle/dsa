89ca38f646ab040a039d51bbc635882f087d0e9b
Saurabh Singh <imsaurabhsingh007@gmail.com>
2025-02-06 21:53:55 +0530

    Solve LRU Cache

--- index.js
+++ index.js
@@ -1,28 +1,69 @@
-class Solution {
+class Node {
+  constructor(key, value) {
+    this.key = key;
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class LRUCache {
   /**
-   * @param {number[]} nums
+   * @param {number} capacity
+   */
+  constructor(capacity) {
+    this.capacity = capacity;
+    this.map = new Map();
+    this.head = new Node();
+    this.tail = new Node();
+    this.head.next = this.tail;
+    this.tail.prev = this.head;
+  }
+
+  /**
+   * @param {number} key
    * @return {number}
    */
-  findDuplicate(nums) {
-    let slow = 0;
-    let fast = 0;
+  get(key) {
+    if (!this.map.has(key)) return -1;
+    const node = this.map.get(key);
+    this.removeNode(node);
+    this.addNode(node);
+    return node.value;
+  }
 
-    do {
-      slow = nums[slow];
-      fast = nums[nums[fast]];
-
-      if (slow === fast) {
-        break;
-      }
-    } while (slow !== fast);
-
-    slow = 0;
-
-    while (slow !== fast) {
-      slow = nums[slow];
-      fast = nums[fast];
+  /**
+   * @param {number} key
+   * @param {number} value
+   * @return {void}
+   */
+  put(key, value) {
+    if (this.map.has(key)) {
+      const node = this.map.get(key);
+      node.value = value;
+      this.removeNode(node);
+      this.addNode(node);
+      return;
     }
+    if (this.map.size >= this.capacity) {
+      const node = this.head.next;
+      this.removeNode(node);
+      this.map.delete(node.key);
+    }
+    const node = new Node(key, value);
+    this.map.set(key, node);
+    this.addNode(node);
+  }
 
-    return slow;
+  removeNode(node) {
+    node.prev.next = node.next;
+    node.next.prev = node.prev;
+  }
+
+  addNode(node) {
+    node.prev = this.tail.prev;
+    node.next = this.tail;
+    this.tail.prev.next = node;
+    this.tail.prev = node;
   }
 }
