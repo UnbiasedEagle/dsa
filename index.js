fedddf00fc6697bde524ec2405d76a805ef48741
Saurabh Singh <imsaurabhsingh007@gmail.com>
2025-02-06 18:44:26 +0530

    Solve Find the Duplicate Number

--- index.js
+++ index.js
@@ -1,36 +1,28 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     constructor(val = 0, next = null) {
- *         this.val = val;
- *         this.next = next;
- *     }
- * }
- */
-
 class Solution {
   /**
-   * @param {ListNode} l1
-   * @param {ListNode} l2
-   * @return {ListNode}
+   * @param {number[]} nums
+   * @return {number}
    */
-  addTwoNumbers(l1, l2) {
-    const dummy = new ListNode();
-    let tail = dummy;
-    let current1 = l1;
-    let current2 = l2;
-    let carry = 0;
+  findDuplicate(nums) {
+    let slow = 0;
+    let fast = 0;
 
-    while (current1 || current2 || carry) {
-      const sum =
-        (current1 ? current1.val : 0) + (current2 ? current2.val : 0) + carry;
-      carry = Math.floor(sum / 10);
-      tail.next = new ListNode(sum % 10);
-      tail = tail.next;
-      current1 = current1 ? current1.next : null;
-      current2 = current2 ? current2.next : null;
+    do {
+      slow = nums[slow];
+      fast = nums[nums[fast]];
+
+      if (slow === fast) {
+        break;
+      }
+    } while (slow !== fast);
+
+    slow = 0;
+
+    while (slow !== fast) {
+      slow = nums[slow];
+      fast = nums[fast];
     }
 
-    return dummy.next;
+    return slow;
   }
 }
