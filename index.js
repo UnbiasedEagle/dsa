ce0b4d57136b99a654251554da1876707734031d
Saurabh Singh <imsaurabhsingh007@gmail.com>
2025-02-08 08:33:57 +0530

    Solve Reverse Nodes in K-Group

--- index.js
+++ index.js
@@ -10,94 +10,41 @@
 
 class Solution {
   /**
-   * @param {ListNode[]} lists
+   * @param {ListNode} head
+   * @param {number} k
    * @return {ListNode}
    */
-  mergeKLists(lists) {
-    const minHeap = new MinHeap();
-    for (let i = 0; i < lists.length; i++) {
-      let node = lists[i];
-      while (node) {
-        minHeap.insert(node);
-        node = node.next;
-      }
-    }
+  reverseKGroup(head, k) {
+    // single node
+    if (!head || !head.next) return head;
 
-    const dummyNode = new ListNode(0);
-    let current = dummyNode;
+    let current = head;
+    let count = 0;
 
-    while (minHeap.heap.length > 0) {
-      const node = minHeap.extractMin();
-      current.next = node;
+    while (current && count < k - 1) {
       current = current.next;
+      count++;
     }
 
-    return dummyNode.next;
-  }
-}
+    if (!current) return head;
 
-class MinHeap {
-  constructor() {
-    this.heap = [];
-  }
-
-  insert(value) {
-    this.heap.push(value);
-    this.bubbleUp(this.heap.length - 1);
+    let nextNode = current.next;
+    current.next = null;
+    let reversed = this.reverseKGroup(nextNode, k);
+    const newHead = this.reverse(head);
+    head.next = reversed;
+    return newHead;
   }
 
-  bubbleUp(index) {
-    while (index > 0) {
-      const parentIndex = Math.floor((index - 1) / 2);
-
-      if (this.heap[index].val < this.heap[parentIndex].val) {
-        const temp = this.heap[index];
-        this.heap[index] = this.heap[parentIndex];
-        this.heap[parentIndex] = temp;
-      }
-
-      index = parentIndex;
+  reverse(head) {
+    let current = head;
+    let prev = null;
+    while (current) {
+      let next = current.next;
+      current.next = prev;
+      prev = current;
+      current = next;
     }
-  }
-
-  extractMin() {
-    if (this.heap.length < 2) {
-      return this.heap.pop();
-    }
-    const min = this.heap[0];
-    this.heap[0] = this.heap.pop();
-    this.bubbleDown(0);
-    return min;
-  }
-
-  bubbleDown(index) {
-    while (true) {
-      let leftChildIndex = 2 * index + 1;
-      let rightChildIndex = 2 * index + 2;
-
-      let minIndex = index;
-
-      if (leftChildIndex < this.heap.length) {
-        if (this.heap[leftChildIndex].val < this.heap[minIndex].val) {
-          minIndex = leftChildIndex;
-        }
-      }
-
-      if (rightChildIndex < this.heap.length) {
-        if (this.heap[rightChildIndex].val < this.heap[minIndex].val) {
-          minIndex = rightChildIndex;
-        }
-      }
-
-      if (minIndex === index) {
-        break;
-      }
-
-      const temp = this.heap[index];
-      this.heap[index] = this.heap[minIndex];
-      this.heap[minIndex] = temp;
-
-      index = minIndex;
-    }
+    return prev;
   }
 }
