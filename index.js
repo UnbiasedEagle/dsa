f0cb6ffd6f5db4c1c393859278b642b41761f3d7
Saurabh Singh <imsaurabhsingh007@gmail.com>
2025-03-05 19:01:13 +0530

    Solve Letter Combinations of a Phone Number

--- index.js
+++ index.js
@@ -1,45 +1,36 @@
 class Solution {
   /**
-   * @param {character[][]} board
-   * @param {string} word
-   * @return {boolean}
+   * @param {string} digits
+   * @return {string[]}
    */
+  letterCombinations(digits) {
+    if (digits.length === 0) return [];
 
-  dfs(board, word, i, j, k) {
-    if (k === word.length) {
-      return true;
-    }
+    const map = {
+      2: ['a', 'b', 'c'],
+      3: ['d', 'e', 'f'],
+      4: ['g', 'h', 'i'],
+      5: ['j', 'k', 'l'],
+      6: ['m', 'n', 'o'],
+      7: ['p', 'q', 'r', 's'],
+      8: ['t', 'u', 'v'],
+      9: ['w', 'x', 'y', 'z'],
+    };
+    const result = [];
 
-    if (
-      i < 0 ||
-      i >= board.length ||
-      j < 0 ||
-      j >= board[i].length ||
-      board[i][j] !== word[k]
-    ) {
-      return false;
-    }
-
-    const char = word[k];
-    board[i][j] = '#';
-    let res =
-      this.dfs(board, word, i + 1, j, k + 1) ||
-      this.dfs(board, word, i - 1, j, k + 1) ||
-      this.dfs(board, word, i, j + 1, k + 1) ||
-      this.dfs(board, word, i, j - 1, k + 1);
-    board[i][j] = char;
-    return res;
-  }
-
-  exist(board, word) {
-    for (let i = 0; i < board.length; i++) {
-      for (let j = 0; j < board[i].length; j++) {
-        if (word[0] === board[i][j] && this.dfs(board, word, i, j, 0)) {
-          return true;
-        }
+    const dfs = (index = 0, str = '') => {
+      if (index === digits.length) {
+        result.push(str);
+        return;
       }
-    }
 
-    return false;
+      for (let i = 0; i < map[digits[index]].length; i++) {
+        dfs(index + 1, str + map[digits[index]][i]);
+      }
+    };
+
+    dfs();
+
+    return result;
   }
 }
