afb254faef557440ef86197c48284cf10de48547
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-01-19 08:02:20 +0530

    feat: add Verifying an Alien Dictionary solution

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 class Solution {
   /**
-   * @param {number[][]} grid
-   * @return {number}
+   * @param {string[]} words
+   * @param {string} order
+   * @return {boolean}
    */
-  islandPerimeter(grid) {
-    const rows = grid.length;
-    const cols = grid[0].length;
+  isAlienSorted(words, order) {
+    const charMap = new Map();
+    for (let i = 0; i < order.length; i++) {
+      charMap.set(order[i], i);
+    }
 
-    const dfs = (r, c) => {
-      if (r < 0 || c < 0 || r >= rows || c >= cols || grid[r][c] === 0) {
-        return 1;
-      }
-      if (grid[r][c] === -1) {
-        return 0;
-      }
-      grid[r][c] = -1;
-      return dfs(r + 1, c) + dfs(r - 1, c) + dfs(r, c + 1) + dfs(r, c - 1);
-    };
+    for (let i = 0; i < words.length - 1; i++) {
+      const word1 = words[i];
+      const word2 = words[i + 1];
 
-    for (let i = 0; i < rows; i++) {
-      for (let j = 0; j < cols; j++) {
-        if (grid[i][j] === 1) {
-          return dfs(i, j);
+      for (let j = 0; j < word1.length; j++) {
+        if (j >= word2.length) return false;
+        if (word1[j] !== word2[j]) {
+          if (charMap.get(word1[j]) > charMap.get(word2[j])) {
+            return false;
+          }
+          break;
         }
       }
     }
+    return true;
   }
 }
 
-// Time Complexity: O(rows * cols)
-// Space Complexity: O(rows * cols)
+// Time Complexity: O(n * m)
+// Space Complexity: O(1)
