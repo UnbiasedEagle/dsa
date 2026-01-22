3360293924ff19fd8d50aa8803b3325a636f6058
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-01-22 13:30:45 +0530

    two sum

--- script.js
+++ script.js
@@ -1,41 +1,23 @@
-/**
- * @param {number} numCourses
- * @param {number[][]} prerequisites
- * @return {boolean}
- */
-var canFinish = function (numCourses, prerequisites) {
-  const graph = Array.from({ length: numCourses }, () => []);
-  const indegrees = Array.from({ length: numCourses }, () => 0);
+class Solution {
+  /**
+   * @param {number[]} nums
+   * @param {number} target
+   * @return {number[]}
+   */
+  twoSum(nums, target) {
+    const map = new Map();
 
-  for (const [course1, course2] of prerequisites) {
-    graph[course2].push(course1);
-    indegrees[course1]++;
-  }
-
-  const queue = [];
-
-  for (let i = 0; i < numCourses; i++) {
-    if (indegrees[i] === 0) {
-      queue.push(i);
-    }
-  }
-
-  let processedCount = 0;
-
-  while (queue.length) {
-    const course = queue.unshift();
-    processedCount++;
-
-    for (const neighbour of graph[course]) {
-      indegrees[neighbour]--;
-      if (indegrees[neighbour] === 0) {
-        queue.push(neighbour);
+    for (let i = 0; i < nums.length; i++) {
+      const comp = target - nums[i];
+      if (map.has(comp)) {
+        return [map.get(comp), i];
       }
+      map.set(nums[i], i);
     }
+
+    return [];
   }
+}
 
-  return processedCount === numCourses;
-};
-
-// Time Complexity: O(V+E)
-// Space Complexity: O(V+E)
+// Time Complexity: O(n)
+// Space Complexity: O(n)
