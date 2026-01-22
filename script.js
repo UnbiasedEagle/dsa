0c2a9ad5d9fdef9860b361f8179c7dcad641a804
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-01-22 10:53:49 +0530

    207. Course Schedule

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
-class Solution {
-  /**
-   * @param {string} s
-   * @param {string} t
-   * @return {boolean}
-   */
-  isAnagram(s, t) {
-    if (s.length !== t.length) {
-      return false;
-    }
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+var canFinish = function (numCourses, prerequisites) {
+  const graph = Array.from({ length: numCourses }, () => []);
+  const indegrees = Array.from({ length: numCourses }, () => 0);
 
-    const map = new Map();
-
-    for (let i = 0; i < s.length; i++) {
-      const char1 = s[i];
-      const char2 = t[i];
-
-      map.set(char1, (map.get(char1) || 0) + 1);
-      map.set(char2, (map.get(char2) || 0) - 1);
-    }
-
-    return Array.from(map.values()).every((entry) => entry === 0);
+  for (const [course1, course2] of prerequisites) {
+    graph[course2].push(course1);
+    indegrees[course1]++;
   }
-}
 
-// Time Complexity: O(n)
-// Space Complexity: O(n)
+  const queue = [];
+
+  for (let i = 0; i < numCourses; i++) {
+    if (indegrees[i] === 0) {
+      queue.push(i);
+    }
+  }
+
+  let processedCount = 0;
+
+  while (queue.length) {
+    const course = queue.unshift();
+    processedCount++;
+
+    for (const neighbour of graph[course]) {
+      indegrees[neighbour]--;
+      if (indegrees[neighbour] === 0) {
+        queue.push(neighbour);
+      }
+    }
+  }
+
+  return processedCount === numCourses;
+};
+
+// Time Complexity: O(V+E)
+// Space Complexity: O(V+E)
