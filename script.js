41dde6d793eee0497046284b0a2b83c43cb0b363
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-01-22 06:58:03 +0530

    Valid Anagram

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 class Solution {
   /**
-   * @param {number[]} nums
+   * @param {string} s
+   * @param {string} t
    * @return {boolean}
    */
-  hasDuplicate(nums) {
-    const set = new Set();
-
-    for (const num of nums) {
-      if (set.has(num)) {
-        return true;
-      }
-      set.add(num);
+  isAnagram(s, t) {
+    if (s.length !== t.length) {
+      return false;
     }
 
-    return false;
+    const map = new Map();
+
+    for (let i = 0; i < s.length; i++) {
+      const char1 = s[i];
+      const char2 = t[i];
+
+      map.set(char1, (map.get(char1) || 0) + 1);
+      map.set(char2, (map.get(char2) || 0) - 1);
+    }
+
+    return Array.from(map.values()).every((entry) => entry === 0);
   }
 }
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
