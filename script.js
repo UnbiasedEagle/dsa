a1067c75d1b49777bb007f91210837af6f2ab470
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-01-23 21:58:16 +0530

    Merge Sorted Array

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
 class Solution {
   /**
-   * @param {string} word1
-   * @param {string} word2
-   * @return {string}
+   * @param {number[]} nums1
+   * @param {number} m
+   * @param {number[]} nums2
+   * @param {number} n
+   * @return {void} Do not return anything, modify nums1 in-place instead.
    */
-  mergeAlternately(word1, word2) {
-    let result = '';
-
+  merge(nums1, m, nums2, n) {
     let i = 0;
-    let j = 0;
+    let j = m - 1;
 
-    while (i < word1.length && j < word2.length) {
-      result += word1[i];
-      result += word2[j];
+    while (i < j) {
+      [nums1[i], nums1[j]] = [nums1[j], nums1[i]];
       i++;
-      j++;
+      j--;
     }
 
-    while (i < word1.length) {
-      result += word1[i++];
+    nums1.reverse();
+
+    let idx = 0;
+    j = 0;
+    i = nums1.length - m;
+
+    while (i < nums1.length && j < nums2.length) {
+      if (nums1[i] < nums2[j]) {
+        nums1[idx] = nums1[i++];
+      } else {
+        nums1[idx] = nums2[j++];
+      }
+      idx++;
     }
-    while (j < word2.length) {
-      result += word2[j++];
+
+    while (j < nums2.length) {
+      nums1[idx] = nums2[j++];
+      idx++;
     }
-    return result;
+    return nums1;
   }
 }
