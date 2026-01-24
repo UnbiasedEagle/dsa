ad28c15a4603f8ca159140aa205ad64ed31e5b7c
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-01-24 16:19:22 +0530

    Design Hashset

--- script.js
+++ script.js
@@ -1,48 +1,79 @@
-class Solution {
-  /**
-   * @param {number[]} nums
-   * @return {number[]}
-   */
-  sortArray(nums) {
-    const left = 0;
-    const right = nums.length - 1;
-    this.sort(nums, left, right);
-    return nums;
-  }
-
-  sort(nums, left, right) {
-    if (left >= right) {
-      return;
-    }
-    const mid = Math.floor((left + right) / 2);
-    this.sort(nums, left, mid);
-    this.sort(nums, mid + 1, right);
-    this.merge(nums, left, mid, right);
-  }
-
-  merge(nums, left, mid, right) {
-    const temp = [];
-
-    let i = left;
-    let j = mid + 1;
-
-    while (i <= mid && j <= right) {
-      if (nums[i] < nums[j]) {
-        temp.push(nums[i++]);
-      } else {
-        temp.push(nums[j++]);
-      }
-    }
-
-    while (i <= mid) {
-      temp.push(nums[i++]);
-    }
-    while (j <= right) {
-      temp.push(nums[j++]);
-    }
-
-    for (let i = 0; i < temp.length; i++) {
-      nums[i + left] = temp[i];
-    }
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
 }
+
+class MyHashSet {
+  constructor() {
+    this.maxSize = 10 ** 4;
+    this.arr = Array.from({ length: this.maxSize }, () => new Node(-1));
+  }
+
+  /**
+   * @param {number} key
+   * @return {void}
+   */
+  add(key) {
+    const idx = key % this.maxSize;
+    let temp = this.arr[idx];
+
+    while (temp.next) {
+      if (temp.value === key) {
+        return;
+      }
+      temp = temp.next;
+    }
+
+    if (temp.value === key) {
+      return;
+    }
+
+    temp.next = new Node(key);
+  }
+
+  /**
+   * @param {number} key
+   * @return {void}
+   */
+  remove(key) {
+    const idx = key % this.maxSize;
+    let temp = this.arr[idx];
+
+    while (temp.next && temp.next.value !== key) {
+      temp = temp.next;
+    }
+
+    if (!temp.next) {
+      return;
+    }
+
+    temp.next = temp.next.next;
+  }
+
+  /**
+   * @param {number} key
+   * @return {boolean}
+   */
+  contains(key) {
+    const idx = key % this.maxSize;
+    let temp = this.arr[idx];
+
+    while (temp) {
+      if (temp.value === key) {
+        return true;
+      }
+      temp = temp.next;
+    }
+    return false;
+  }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * var obj = new MyHashSet()
+ * obj.add(key)
+ * obj.remove(key)
+ * var param_3 = obj.contains(key)
+ */
