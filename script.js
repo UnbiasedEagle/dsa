9f72d3dc82bc8bfc443a57694401438ded150bd5
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-01-25 07:16:11 +0530

    Range Sum Query 2D Immutable

--- script.js
+++ script.js
@@ -1,37 +1,51 @@
-class Solution {
+class NumMatrix {
   /**
-   * @param {string[]} strs
-   * @returns {string}
+   * @param {number[][]} matrix
    */
-  encode(strs) {
-    let result = "";
-    for (const str of strs) {
-      result += str.length + "#" + str;
+  constructor(matrix) {
+    const rows = matrix.length;
+    const cols = matrix[0].length;
+
+    this.store = Array.from({ length: rows + 1 }, () =>
+      Array.from({ length: cols + 1 }, () => 0),
+    );
+
+    for (let i = 1; i <= rows; i++) {
+      for (let j = 1; j <= cols; j++) {
+        this.store[i][j] =
+          matrix[i - 1][j - 1] +
+          this.store[i - 1][j] +
+          this.store[i][j - 1] -
+          this.store[i - 1][j - 1];
+      }
     }
-    return result;
   }
 
   /**
-   * @param {string} str
-   * @returns {string[]}
+   * @param {number} row1
+   * @param {number} col1
+   * @param {number} row2
+   * @param {number} col2
+   * @return {number}
    */
-  decode(str) {
-    const result = [];
+  sumRegion(row1, col1, row2, col2) {
+    row1 = row1 + 1;
+    col1 = col1 + 1;
+    row2 = row2 + 1;
+    col2 = col2 + 1;
 
-    let i = 0;
-
-    while (i < str.length) {
-      let j = i;
-      let length = "";
-      while (str[j] !== "#") {
-        length += str[j];
-        j++;
-      }
-      j++;
-      result.push(str.slice(j, j + +length));
-      i = j + +length;
-    }
+    const result =
+      this.store[row2][col2] -
+      this.store[row1 - 1][col2] -
+      this.store[row2][col1 - 1] +
+      this.store[row1 - 1][col1 - 1];
 
     return result;
   }
 }
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * var obj = new NumMatrix(matrix)
+ * var param_1 = obj.sumRegion(row1,col1,row2,col2)
+ */
