1c6585e35167bf193be877ab556b215723bce02a
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-01-25 12:31:04 +0530

    Valid Sudoku

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
 class Solution {
   /**
-   * @param {number[]} nums
-   * @return {number[]}
+   * @param {character[][]} board
+   * @return {boolean}
    */
-  productExceptSelf(nums) {
-    const result = Array.from({ length: nums.length }, () => 1);
+  isValidSudoku(board) {
+    const rowsSet = Array.from({ length: 9 }, () => new Set());
+    const cols = Array.from({ length: 9 }, () => new Set());
+    const grid = Array.from({ length: 3 }, () =>
+      Array.from({ length: 3 }, () => new Set()),
+    );
 
-    let rightProd = 1;
-    for (let i = nums.length - 1; i >= 0; i--) {
-      result[i] = rightProd;
-      rightProd *= nums[i];
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        if (board[i][j] !== ".") {
+          if (rowsSet[i].has(board[i][j])) {
+            return false;
+          }
+          if (cols[j].has(board[i][j])) {
+            return false;
+          }
+          const gridRow = Math.floor(i / 3);
+          const gridCol = Math.floor(j / 3);
+
+          if (grid[gridRow][gridCol].has(board[i][j])) {
+            return false;
+          }
+
+          cols[j].add(board[i][j]);
+          rowsSet[i].add(board[i][j]);
+          grid[gridRow][gridCol].add(board[i][j]);
+        }
+      }
     }
 
-    let leftProd = 1;
-
-    for (let i = 0; i < nums.length; i++) {
-      result[i] *= leftProd;
-      leftProd *= nums[i];
-    }
-
-    return result;
+    return true;
   }
 }
