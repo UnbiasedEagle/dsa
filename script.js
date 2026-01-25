57329c04e1e26c9133d88a53260b4a08d2e25251
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-01-25 07:42:33 +0530

    Products of Array Except Self

--- script.js
+++ script.js
@@ -1,51 +1,24 @@
-class NumMatrix {
+class Solution {
   /**
-   * @param {number[][]} matrix
+   * @param {number[]} nums
+   * @return {number[]}
    */
-  constructor(matrix) {
-    const rows = matrix.length;
-    const cols = matrix[0].length;
+  productExceptSelf(nums) {
+    const result = Array.from({ length: nums.length }, () => 1);
 
-    this.store = Array.from({ length: rows + 1 }, () =>
-      Array.from({ length: cols + 1 }, () => 0),
-    );
-
-    for (let i = 1; i <= rows; i++) {
-      for (let j = 1; j <= cols; j++) {
-        this.store[i][j] =
-          matrix[i - 1][j - 1] +
-          this.store[i - 1][j] +
-          this.store[i][j - 1] -
-          this.store[i - 1][j - 1];
-      }
+    let rightProd = 1;
+    for (let i = nums.length - 1; i >= 0; i--) {
+      result[i] = rightProd;
+      rightProd *= nums[i];
     }
-  }
 
-  /**
-   * @param {number} row1
-   * @param {number} col1
-   * @param {number} row2
-   * @param {number} col2
-   * @return {number}
-   */
-  sumRegion(row1, col1, row2, col2) {
-    row1 = row1 + 1;
-    col1 = col1 + 1;
-    row2 = row2 + 1;
-    col2 = col2 + 1;
+    let leftProd = 1;
 
-    const result =
-      this.store[row2][col2] -
-      this.store[row1 - 1][col2] -
-      this.store[row2][col1 - 1] +
-      this.store[row1 - 1][col1 - 1];
+    for (let i = 0; i < nums.length; i++) {
+      result[i] *= leftProd;
+      leftProd *= nums[i];
+    }
 
     return result;
   }
 }
-
-/**
- * Your NumMatrix object will be instantiated and called as such:
- * var obj = new NumMatrix(matrix)
- * var param_1 = obj.sumRegion(row1,col1,row2,col2)
- */
