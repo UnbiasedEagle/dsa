af0e091d7ba8603a16d8b355bdddbc1f53097a37
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-01-26 21:19:51 +0530

    Majority Element II

--- script.js
+++ script.js
@@ -1,15 +1,49 @@
 class Solution {
   /**
-   * @param {number[]} prices
-   * @return {number}
+   * @param {number[]} nums
+   * @return {number[]}
    */
-  maxProfit(prices) {
-    let result = 0;
+  majorityElement(nums) {
+    let count1 = 0;
+    let count2 = 0;
+    let num1 = nums[0];
+    let num2 = nums[0];
 
-    for (let i = 1; i < prices.length; i++) {
-      if (prices[i] > prices[i - 1]) {
-        result += prices[i] - prices[i - 1];
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] === num1) {
+        count1++;
+      } else if (nums[i] === num2) {
+        count2++;
+      } else if (count1 === 0) {
+        num1 = nums[i];
+        count1 = 1;
+      } else if (count2 === 0) {
+        num2 = nums[i];
+        count2 = 1;
+      } else {
+        count1--;
+        count2--;
       }
+    }
+
+    count1 = 0;
+    count2 = 0;
+
+    for (const num of nums) {
+      if (num === num1) {
+        count1++;
+      } else if (num === num2) {
+        count2++;
+      }
+    }
+
+    const result = [];
+
+    if (count1 > Math.floor(nums.length / 3)) {
+      result.push(num1);
+    }
+    if (count2 > Math.floor(nums.length / 3)) {
+      result.push(num2);
     }
 
     return result;
