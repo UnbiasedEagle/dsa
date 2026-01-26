5acc74d4a7306e6198b3395a5a887335696ab934
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-01-26 12:34:06 +0530

    3Sum

--- script.js
+++ script.js
@@ -1,27 +1,46 @@
 class Solution {
   /**
-   * @param {number[]} numbers
-   * @param {number} target
-   * @return {number[]}
+   * @param {number[]} nums
+   * @return {number[][]}
    */
-  twoSum(nums, target) {
-    let start = 0;
-    let end = nums.length - 1;
+  threeSum(nums) {
+    nums.sort((a, b) => a - b);
 
-    while (start < end) {
-      const total = nums[start] + nums[end];
+    let i = 0;
+    const result = [];
+    while (i < nums.length) {
+      let start = i + 1;
+      let end = nums.length - 1;
 
-      if (total === target) {
-        return [start + 1, end + 1];
+      while (start < end) {
+        const total = nums[i] + nums[start] + nums[end];
+
+        if (total === 0) {
+          result.push([nums[i], nums[start], nums[end]]);
+          start++;
+          end--;
+
+          while (start < end && nums[start] === nums[start - 1]) {
+            start++;
+          }
+
+          while (start < end && nums[end] === nums[end + 1]) {
+            end--;
+          }
+        } else if (total > 0) {
+          end--;
+        } else {
+          start++;
+        }
       }
 
-      if (total > target) {
-        end--;
-      } else {
-        start++;
+      i++;
+
+      while (i < nums.length && nums[i] === nums[i - 1]) {
+        i++;
       }
     }
 
-    return [];
+    return result;
   }
 }
