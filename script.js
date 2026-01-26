0eb8e9fdd771b28722feae6b2b117f64c293daae
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-01-26 13:05:00 +0530

    Longest Consecutive Sequence

--- script.js
+++ script.js
@@ -1,59 +1,30 @@
 class Solution {
   /**
    * @param {number[]} nums
-   * @param {number} target
-   * @return {number[][]}
+   * @return {number}
    */
-  fourSum(nums, target) {
-    nums.sort((a, b) => a - b);
+  longestConsecutive(nums) {
+    const uniqueNums = new Set(nums);
+
+    let maxLength = 0;
 
     let i = 0;
 
-    const result = [];
-
     while (i < nums.length) {
-      let j = i + 1;
-      while (j < nums.length) {
-        let start = j + 1;
-        let end = nums.length - 1;
+      if (!uniqueNums.has(nums[i] - 1)) {
+        let length = 0;
+        let num = nums[i];
 
-        while (start < end) {
-          const total = nums[i] + nums[j] + nums[start] + nums[end];
-
-          if (total === target) {
-            result.push([nums[i], nums[j], nums[start], nums[end]]);
-
-            start++;
-            end--;
-
-            while (start < end && nums[start] === nums[start - 1]) {
-              start++;
-            }
-
-            while (start < end && nums[end] === nums[end + 1]) {
-              end--;
-            }
-          } else if (total > target) {
-            end--;
-          } else {
-            start++;
-          }
+        while (uniqueNums.has(num)) {
+          length++;
+          num++;
         }
 
-        j++;
-
-        while (j < nums.length && nums[j] === nums[j - 1]) {
-          j++;
-        }
+        maxLength = Math.max(maxLength, length);
       }
-
       i++;
-
-      while (i < nums.length && nums[i] === nums[i - 1]) {
-        i++;
-      }
     }
 
-    return result;
+    return maxLength;
   }
 }
