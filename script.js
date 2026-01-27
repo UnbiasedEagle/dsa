78242e058f4b15746a5f8e4e69299f8e4ce92a1f
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-01-27 07:12:18 +0530

    Subarray Sum Equals K

--- script.js
+++ script.js
@@ -1,51 +1,28 @@
 class Solution {
   /**
    * @param {number[]} nums
-   * @return {number[]}
+   * @param {number} k
+   * @return {number}
    */
-  majorityElement(nums) {
-    let count1 = 0;
-    let count2 = 0;
-    let num1 = nums[0];
-    let num2 = nums[0];
+  subarraySum(nums, k) {
+    let sum = 0;
+    const sumMap = new Map();
+    let count = 0;
 
     for (let i = 0; i < nums.length; i++) {
-      if (nums[i] === num1) {
-        count1++;
-      } else if (nums[i] === num2) {
-        count2++;
-      } else if (count1 === 0) {
-        num1 = nums[i];
-        count1 = 1;
-      } else if (count2 === 0) {
-        num2 = nums[i];
-        count2 = 1;
-      } else {
-        count1--;
-        count2--;
+      sum += nums[i];
+
+      if (sum === k) {
+        count++;
       }
+
+      const comp = sum - k;
+
+      count += map.get(comp) || 0;
+
+      map.set(sum, (map.get(sum) || 0) + 1);
     }
 
-    count1 = 0;
-    count2 = 0;
-
-    for (const num of nums) {
-      if (num === num1) {
-        count1++;
-      } else if (num === num2) {
-        count2++;
-      }
-    }
-
-    const result = [];
-
-    if (count1 > Math.floor(nums.length / 3)) {
-      result.push(num1);
-    }
-    if (count2 > Math.floor(nums.length / 3)) {
-      result.push(num2);
-    }
-
-    return result;
+    return count;
   }
 }
