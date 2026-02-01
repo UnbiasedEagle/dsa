f8a70ed711ceecb12182de29abc0d37e51761e4e
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-02-01 18:34:37 +0530

    Implement Stack Using Queues

--- script.js
+++ script.js
@@ -1,36 +1,52 @@
-class Solution {
+class MyStack {
+  constructor() {
+    this.queue1 = [];
+    this.queue2 = [];
+  }
+
   /**
-   * @param {number[]} nums
-   * @param {number} target
+   * @param {number} x
+   * @return {void}
+   */
+  push(x) {
+    this.queue2.push(x);
+
+    while (this.queue1.length > 0) {
+      this.queue2.push(this.queue1.shift());
+    }
+
+    while (this.queue2.length > 0) {
+      this.queue1.push(this.queue2.shift());
+    }
+  }
+
+  /**
    * @return {number}
    */
-  searchInsert(nums, target) {
-    if (target < nums[0]) {
-      return 0;
-    }
-    if (target > nums[nums.length - 1]) {
-      return nums.length;
-    }
+  pop() {
+    return this.queue1.shift();
+  }
 
-    let start = 0;
-    let end = nums.length - 1;
-    let result = nums.length;
+  /**
+   * @return {number}
+   */
+  top() {
+    return this.queue1[0];
+  }
 
-    while (start <= end) {
-      const mid = Math.floor((start + end) / 2);
-
-      if (nums[mid] === target) {
-        return mid;
-      }
-
-      if (nums[mid] > target) {
-        result = mid;
-        end = mid - 1;
-      } else {
-        start = mid + 1;
-      }
-    }
-
-    return result;
+  /**
+   * @return {boolean}
+   */
+  empty() {
+    return this.queue1.length === 0;
   }
 }
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = new MyStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
