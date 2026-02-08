03a5b74f43947042d204c37cd4c631081228fd49
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-02-08 16:36:42 +0530

    Merge Two Sorted Linked Lists

--- script.js
+++ script.js
@@ -10,20 +10,32 @@
 
 class Solution {
   /**
-   * @param {ListNode} head
+   * @param {ListNode} list1
+   * @param {ListNode} list2
    * @return {ListNode}
    */
-  reverseList(head) {
-    let curr = head;
-    let prev = null;
+  mergeTwoLists(list1, list2) {
+    let dummy = new ListNode();
+    let tail = dummy;
 
-    while (curr) {
-      const next = curr.next;
-      curr.next = prev;
-      prev = curr;
-      curr = next;
+    while (list1 && list2) {
+      if (list1.val < list2.val) {
+        tail.next = list1;
+        list1 = list1.next;
+      } else {
+        tail.next = list2;
+        list2 = list2.next;
+      }
+      tail = tail.next;
     }
 
-    return prev;
+    if (list1) {
+      tail.next = list1;
+    }
+    if (list2) {
+      tail.next = list2;
+    }
+
+    return dummy.next;
   }
 }
