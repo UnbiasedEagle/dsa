9576fb2dc2c1228c6b52259be05e3fc4affe2f87
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-02-08 12:42:15 +0530

    Search a 2D Matrix

--- script.js
+++ script.js
@@ -1,25 +1,52 @@
 class Solution {
   /**
-   * @param {number} x
-   * @return {number}
+   * @param {number[][]} matrix
+   * @param {number} target
+   * @return {boolean}
    */
-  mySqrt(x) {
-    let start = 0;
-    let end = x;
+  searchMatrix(matrix, target) {
+    let rows = matrix.length;
+    let cols = matrix[0].length;
 
-    let result = x;
+    let rs = 0;
+    let re = rows - 1;
 
-    while (start <= end) {
-      const mid = Math.floor((start + end) / 2);
+    let r = -1;
 
-      if (mid * mid <= x) {
-        result = mid;
-        start = mid + 1;
+    while (rs <= re) {
+      const mid = Math.floor((rs + re) / 2);
+
+      if (target >= matrix[mid][0] && target <= matrix[mid][cols - 1]) {
+        r = mid;
+        break;
+      } else if (target > matrix[mid][cols - 1]) {
+        rs = mid + 1;
       } else {
-        end = mid - 1;
+        re = mid - 1;
       }
     }
 
-    return result;
+    if (r === -1) {
+      return false;
+    }
+
+    let cs = 0;
+    let ce = cols - 1;
+
+    while (cs <= ce) {
+      const mid = Math.floor((cs + ce) / 2);
+
+      if (matrix[r][mid] === target) {
+        return true;
+      }
+
+      if (matrix[r][mid] > target) {
+        ce = mid - 1;
+      } else {
+        cs = mid + 1;
+      }
+    }
+
+    return false;
   }
 }
