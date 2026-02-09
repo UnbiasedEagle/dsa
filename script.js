5647b57a6f96fa9fd5af88f33b1d1ef54c87662f
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-02-09 10:56:29 +0530

    Minimum Window Substring

--- script.js
+++ script.js
@@ -1,31 +1,54 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     constructor(val = 0, next = null) {
- *         this.val = val;
- *         this.next = next;
- *     }
- * }
- */
-
 class Solution {
   /**
-   * @param {ListNode} head
-   * @return {boolean}
+   * @param {string} s
+   * @param {string} t
+   * @return {string}
    */
-  hasCycle(head) {
-    let slow = head;
-    let fast = head;
+  minWindow(s, t) {
+    const reqMap = new Map();
 
-    while (fast && fast.next) {
-      slow = slow.next;
-      fast = fast.next.next;
+    for (const char of t) {
+      reqMap.set(char, (reqMap.get(char) || 0) + 1);
+    }
 
-      if (slow === fast) {
-        return true;
+    const windowMap = new Map();
+
+    let ws = 0;
+    let we = 0;
+    let minLength = Infinity;
+    let start = -1;
+    while (we < s.length) {
+      windowMap.set(s[we], (windowMap.get(s[we]) || 0) + 1);
+
+      while (ws <= we && this.isValidWindow(windowMap, reqMap)) {
+        const length = we - ws + 1;
+
+        if (length < minLength) {
+          minLength = length;
+          start = ws;
+        }
+
+        windowMap.set(s[ws], windowMap.get(s[ws]) - 1);
+        ws++;
+      }
+
+      we++;
+    }
+
+    if (minLength === Infinity) {
+      return "";
+    }
+
+    return s.slice(start, start + minLength);
+  }
+
+  isValidWindow(map1, map2) {
+    for (const [key, value] of map2.entries()) {
+      if (!map1.has(key) || map1.get(key) < value) {
+        return false;
       }
     }
 
-    return false;
+    return true;
   }
 }
