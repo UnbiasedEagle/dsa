36ccdd855af809afe91912d00b6afd78060eb091
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-02-09 11:16:22 +0530

    Sliding Window Maximum

--- script.js
+++ script.js
@@ -1,54 +1,29 @@
 class Solution {
   /**
-   * @param {string} s
-   * @param {string} t
-   * @return {string}
+   * @param {number[]} nums
+   * @param {number} k
+   * @return {number[]}
    */
-  minWindow(s, t) {
-    const reqMap = new Map();
+  maxSlidingWindow(nums, k) {
+    const queue = [];
+    const result = [];
 
-    for (const char of t) {
-      reqMap.set(char, (reqMap.get(char) || 0) + 1);
-    }
-
-    const windowMap = new Map();
-
-    let ws = 0;
-    let we = 0;
-    let minLength = Infinity;
-    let start = -1;
-    while (we < s.length) {
-      windowMap.set(s[we], (windowMap.get(s[we]) || 0) + 1);
-
-      while (ws <= we && this.isValidWindow(windowMap, reqMap)) {
-        const length = we - ws + 1;
-
-        if (length < minLength) {
-          minLength = length;
-          start = ws;
-        }
-
-        windowMap.set(s[ws], windowMap.get(s[ws]) - 1);
-        ws++;
+    for (let i = 0; i < nums.length; i++) {
+      while (queue.length > 0 && i - queue[0] >= k) {
+        queue.shift();
       }
 
-      we++;
-    }
+      while (queue.length > 0 && nums[queue[queue.length - 1]] <= nums[i]) {
+        queue.pop();
+      }
 
-    if (minLength === Infinity) {
-      return "";
-    }
+      queue.push(i);
 
-    return s.slice(start, start + minLength);
-  }
-
-  isValidWindow(map1, map2) {
-    for (const [key, value] of map2.entries()) {
-      if (!map1.has(key) || map1.get(key) < value) {
-        return false;
+      if (i >= k - 1) {
+        result.push(nums[queue[0]]);
       }
     }
 
-    return true;
+    return result;
   }
 }
