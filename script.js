b0d9dbd647a4068b218b666974565b897f3d2045
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-02-11 08:14:02 +0530

    Make Counter

--- script.js
+++ script.js
@@ -1,31 +1,26 @@
-class StockSpanner {
-  constructor() {
-    this.stockPrices = [];
-  }
+function makeCounter(initialValue = 0) {
+  let counter = initialValue;
 
-  /**
-   * @param {number} price
-   * @return {number}
-   */
-  next(price) {
-    let days = 1;
-
-    while (
-      this.stockPrices.length > 0 &&
-      this.stockPrices[this.stockPrices.length - 1][0] <= price
-    ) {
-      days += this.stockPrices[this.stockPrices.length - 1][1];
-      this.stockPrices.pop();
-    }
-
-    this.stockPrices.push([price, days]);
-
-    return days;
-  }
+  return {
+    increment() {
+      counter++;
+      return counter;
+    },
+    decrement() {
+      counter--;
+      return counter;
+    },
+    reset() {
+      counter = initialValue;
+      return counter;
+    },
+  };
 }
 
-/**
- * Your StockSpanner object will be instantiated and called as such:
- * var obj = new StockSpanner()
- * var param_1 = obj.next(price)
- */
+const counter = makeCounter(5);
+
+console.log(counter.increment());
+console.log(counter.increment());
+console.log(counter.decrement());
+console.log(counter.reset());
+console.log(counter.decrement());
