5f8af74adace202bd09336d0435d8eff406778da
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-02-11 16:48:44 +0530

    Count negative

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
-/**
- * @param {number} x
- * @return {number}
- */
-var reverse = function (x) {
-  let copy = Math.abs(x);
-  let reverse = 0;
-  const INT_MAX = Math.pow(2, 31) - 1;
-  const INT_MIN = Math.pow(2, 31) * -1;
+function countNegatives(arr) {
+  if (!Array.isArray(arr)) {
+    return false;
+  }
+  let result = 0;
 
-  while (copy > 0) {
-    if (
-      reverse > Math.trunc(INT_MAX / 10) ||
-      reverse < Math.trunc(INT_MIN / 10)
-    ) {
-      return 0;
+  for (const num of arr) {
+    if (!Number.isFinite(num)) {
+      return false;
     }
-    reverse = reverse * 10 + (copy % 10);
-    copy = Math.floor(copy / 10);
+    if (num < 0) {
+      result++;
+    }
   }
 
-  return x < 0 ? -reverse : reverse;
-};
+  return result;
+}
+
+module.exports = { countNegatives };
