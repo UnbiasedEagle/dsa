7d71c7b048596d9104f430c62be597fb0fec0296
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-02-11 19:21:53 +0530

    704. Binary Search

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
-function countNegatives(arr) {
-  if (!Array.isArray(arr)) {
-    return false;
-  }
-  let result = 0;
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+  let start = 0;
+  let end = nums.length - 1;
 
-  for (const num of arr) {
-    if (!Number.isFinite(num)) {
-      return false;
+  while (start <= end) {
+    const mid = Math.floor((start + end) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
     }
-    if (num < 0) {
-      result++;
+
+    if (nums[mid] > target) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
     }
   }
 
-  return result;
-}
-
-module.exports = { countNegatives };
+  return -1;
+};
