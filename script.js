2225c068ee17a8247a1b20203199080844f917ee
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-02-11 13:57:03 +0530

    7. Reverse Integer

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 /**
  * @param {number} x
- * @return {boolean}
+ * @return {number}
  */
-var isPalindrome = function (x) {
-  if (x < 0) {
-    return false;
-  }
+var reverse = function (x) {
+  let copy = Math.abs(x);
   let reverse = 0;
-  let copy = x;
+  const INT_MAX = Math.pow(2, 31) - 1;
+  const INT_MIN = Math.pow(2, 31) * -1;
 
   while (copy > 0) {
+    if (
+      reverse > Math.trunc(INT_MAX / 10) ||
+      reverse < Math.trunc(INT_MIN / 10)
+    ) {
+      return 0;
+    }
     reverse = reverse * 10 + (copy % 10);
     copy = Math.floor(copy / 10);
   }
 
-  return reverse === x;
+  return x < 0 ? -reverse : reverse;
 };
