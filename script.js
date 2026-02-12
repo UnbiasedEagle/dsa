ff5d81706e721628eaad2ff0da335862d5d818fa
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-02-12 17:04:48 +0530

    26. Remove Duplicates from Sorted Array

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 /**
- * @param {number} n
- * @return {boolean}
+ * @param {number[]} nums
+ * @return {number}
  */
-var isPowerOfTwo = function (n) {
-  if (n <= 0) {
-    return false;
+var removeDuplicates = function (nums) {
+  let uniqueCount = 1;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] !== nums[uniqueCount - 1]) {
+      nums[uniqueCount] = nums[i];
+      uniqueCount++;
+    }
   }
-  return (n & (n - 1)) === 0;
+
+  return uniqueCount;
 };
