b26ce4ed23a8f6d45a20031ae3374e66d299390b
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-02-12 13:49:45 +0530

    231. Power of Two

--- script.js
+++ script.js
@@ -1,50 +1,10 @@
 /**
- * @param {number[]} nums
- * @return {number[]}
+ * @param {number} n
+ * @return {boolean}
  */
-var sortArray = function (nums) {
-  mergeSort(nums, 0, nums.length - 1);
-  return nums;
+var isPowerOfTwo = function (n) {
+  if (n <= 0) {
+    return false;
+  }
+  return (n & (n - 1)) === 0;
 };
-
-function mergeSort(nums, start, end) {
-  if (start >= end) {
-    return;
-  }
-  const mid = Math.floor((start + end) / 2);
-
-  mergeSort(nums, start, mid);
-  mergeSort(nums, mid + 1, end);
-
-  merge(nums, start, mid, end);
-}
-
-function merge(nums, start, mid, end) {
-  const temp = [];
-  let i = start;
-  let j = mid + 1;
-
-  while (i <= mid && j <= end) {
-    if (nums[i] < nums[j]) {
-      temp.push(nums[i]);
-      i++;
-    } else {
-      temp.push(nums[j]);
-      j++;
-    }
-  }
-
-  while (i <= mid) {
-    temp.push(nums[i]);
-    i++;
-  }
-
-  while (j <= end) {
-    temp.push(nums[j]);
-    j++;
-  }
-
-  for (let i = 0; i < temp.length; i++) {
-    nums[start + i] = temp[i];
-  }
-}
