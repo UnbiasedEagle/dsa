5eb611a159a2b6fdfcc08c5f1a54d77c48e7e5b8
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-02-13 16:14:00 +0530

    136. Single Number

--- script.js
+++ script.js
@@ -2,15 +2,11 @@
  * @param {number[]} nums
  * @return {number}
  */
-var missingNumber = function (nums) {
+var singleNumber = function (nums) {
   let result = 0;
 
   for (const num of nums) {
     result ^= num;
-  }
-
-  for (let i = 1; i <= nums.length; i++) {
-    result ^= i;
   }
 
   return result;
