edbf0246569e9665839d61195f17e614212ee8b9
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-02-13 11:01:27 +0530

    Custom sort

--- script.js
+++ script.js
@@ -1,23 +1,14 @@
-class Solution {
-  /**
-   * @param {string} path
-   * @return {string}
-   */
-  simplifyPath(path) {
-    const dirs = path.split("/").filter((el) => el.length > 0);
+function customSort(arr) {
+  const charArr = arr.filter((item) => isNaN(item));
+  const numArr = arr.filter((item) => !isNaN(item));
 
-    const stack = [];
+  charArr.sort((a, b) => a.charCodeAt(0) - b.charCodeAt(0));
+  numArr.sort((a, b) => a - b);
 
-    for (const dir of dirs) {
-      if (dir === ".") {
-        continue;
-      } else if (dir === "..") {
-        stack.pop();
-      } else {
-        stack.push(dir);
-      }
-    }
-
-    return "/" + stack.join("/");
-  }
+  return [...charArr, ...numArr];
 }
+
+const input = ["g", "s", 5, 2, "c", "e", 6, 1, "a"];
+console.log(customSort(input));
+
+// module.exports = customSort;
