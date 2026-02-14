1e5158785150209032b9e0e27938d92137e695fe
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-02-14 10:55:53 +0530

    Decode String

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
-var singleNumber = function (nums) {
-  let result = 0;
+class Solution {
+  /**
+   * @param {string} s
+   * @return {string}
+   */
+  decodeString(s) {
+    const stack = [];
 
-  for (const num of nums) {
-    result ^= num;
+    for (let i = 0; i < s.length; i++) {
+      if (s[i] === "]") {
+        let word = "";
+
+        while (stack.length > 0 && stack[stack.length - 1] !== "[") {
+          word = stack.pop() + word;
+        }
+
+        stack.pop();
+        let count = "";
+        while (stack.length > 0 && !isNaN(stack[stack.length - 1])) {
+          count = stack.pop() + count;
+        }
+        count = +count;
+        let decodedString = "";
+        for (let i = 0; i < count; i++) {
+          decodedString += word;
+        }
+        stack.push(decodedString);
+      } else {
+        stack.push(s[i]);
+      }
+    }
+
+    return stack.join("");
   }
-
-  return result;
-};
+}
