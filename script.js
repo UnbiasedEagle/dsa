6dd15f2b0313422265fd3449aae5f3897d24f7c9
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-02-16 08:28:25 +0530

    cycle detection using bfs

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
-// Detect cycle in a directed graph using dfs
+// Kahn Algorithm -> topological ordering && cycle detection using bfs
 
-function cycleDetectionUsingDFS(graph) {
-  const visited = new Set();
-  const recStack = new Set();
+function cycleDetectionUsingBFS(graph, n) {
+  const indegree = Array(n).fill(0);
 
-  function dfs(node) {
-    if (recStack.has(node)) {
-      return true;
+  for (let i = 0; i < n; i++) {
+    const neighbours = graph[i] || [];
+
+    for (const neighbour of neighbours) {
+      indegree[neighbour]++;
     }
-    if (visited.has(node)) {
-      return false;
+  }
+
+  const queue = [];
+
+  for (let i = 0; i < n; i++) {
+    if (indegree[i] === 0) {
+      queue.push(i);
     }
-    visited.add(node);
-    recStack.add(node);
+  }
+
+  let processedNodes = 0;
+
+  while (queue.length > 0) {
+    const node = queue.shift();
+    processedNodes++;
 
     for (const neighbour of graph[node]) {
-      if (dfs(neighbour)) {
-        return true;
+      indegree[neighbour]--;
+      if (indegree[neighbour] === 0) {
+        queue.push(neighbour);
       }
     }
-
-    recStack.delete(node);
-    return false;
   }
+
+  return processedNodes !== n;
 }
