64cf4a25c10efd6f14b9f7c59598c6cd0948ea72
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-02-16 10:20:31 +0530

    746. Min Cost Climbing Stairs

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-// fibonacci number
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function (cost) {
+  const dp = Array(cost.length);
+  dp[0] = cost[0];
+  dp[1] = cost[1];
 
-function fib(n, dp) {
-  if (n <= 1) {
-    return n;
+  for (let i = 2; i < cost.length; i++) {
+    dp[i] = Math.min(dp[i - 1], dp[i - 2]) + cost[i];
   }
 
-  if (dp[n] !== -1) {
-    return dp[n];
-  }
-
-  dp[n] = fib(n - 1, dp) + fib(n - 2, dp);
-
-  return dp[n];
-}
+  return Math.min(dp[cost.length - 1], dp[cost.length - 2]);
+};
