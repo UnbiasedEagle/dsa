a1c0e552e9e72af2a533efb3d6102578d98cc85b
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-02-16 06:33:31 +0530

    find all paths from src to target

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
-function dfs(graph, node, visited, result) {
-  result.push(node);
-  visited.add(node);
+// All paths from src to target
 
-  for (const neighbour of graph[node]) {
-    if (!visited.has(neighbour)) {
-      dfs(graph, neighbour, visited, result);
-    }
+function findAllPaths(graph, currNode, visted, target, currPath, result) {
+  if (currNode === target) {
+    result.push([...currPath]);
+    return;
   }
+  if (visted.has(currNode)) {
+    return;
+  }
+
+  visted.add(currNode);
+
+  for (const neighbour of graph[currNode]) {
+    currPath.push(neighbour);
+    findAllPaths(graph, neighbour, visted, target, currPath, result);
+    currPath.pop();
+  }
+
+  visted.delete(currNode);
 }
+
+const result = [];
+const graph = {
+  0: [1, 4, 2],
+  1: [3],
+  2: [3],
+  3: [],
+  4: [2],
+};
+
+findAllPaths(graph, 0, new Set(), 3, [0], result);
+
+console.log(result);
