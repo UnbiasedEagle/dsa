e3906d582d8de1140bd3f35cee3931986f30c9f0
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-02-16 07:54:09 +0530

    path exists

--- script.js
+++ script.js
@@ -1,42 +1,18 @@
-/**
- * @param {character[][]} grid
- * @return {number}
- */
-var numIslands = function (grid) {
-  let result = 0;
+// Find if path exists
 
-  for (let i = 0; i < grid.length; i++) {
-    for (let j = 0; j < grid[0].length; j++) {
-      if (grid[i][j] === "1") {
-        result++;
-        dfs(grid, i, j);
+function isPathExists(graph, node, dest, visited) {
+  if (node === dest) {
+    return true;
+  }
+  visited.add(node);
+
+  for (const neighbour of graph[node]) {
+    if (!visited.has(neighbour)) {
+      if (isPathExists(graph, neighbour, dest, visited)) {
+        return true;
       }
     }
   }
 
-  return result;
-};
-
-function dfs(grid, i, j) {
-  if (
-    i < 0 ||
-    j < 0 ||
-    i >= grid.length ||
-    j >= grid[0].length ||
-    grid[i][j] === "0"
-  ) {
-    return;
-  }
-  grid[i][j] = "0";
-
-  const dirs = [
-    [1, 0],
-    [-1, 0],
-    [0, 1],
-    [0, -1],
-  ];
-
-  for (const dir of dirs) {
-    dfs(grid, i + dir[0], j + dir[1]);
-  }
+  return false;
 }
