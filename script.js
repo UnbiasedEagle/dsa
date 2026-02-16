c0e8cfeff7741504541d0a6f14bb07fbe057f599
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-02-16 10:43:37 +0530

    213. House Robber II

--- script.js
+++ script.js
@@ -6,6 +6,16 @@ var rob = function (nums) {
   if (nums.length === 1) {
     return nums[0];
   }
+  return Math.max(
+    helper(nums.slice(0, nums.length - 1)),
+    helper(nums.slice(1, nums.length)),
+  );
+};
+
+function helper(nums) {
+  if (nums.length === 1) {
+    return nums[0];
+  }
   const dp = Array(nums.length);
   dp[0] = nums[0];
   dp[1] = Math.max(nums[0], nums[1]);
@@ -15,4 +25,4 @@ var rob = function (nums) {
   }
 
   return dp[nums.length - 1];
-};
+}
