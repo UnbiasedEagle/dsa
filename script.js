97791ae7b5a53cf0eb7e9e6a62ad9f48d4effad8
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-02-16 08:03:21 +0530

    find town judge

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-// Find if path exists
+// Find town judge -> indegree = n-1 and outdegree = 0
 
-function isPathExists(graph, node, dest, visited) {
-  if (node === dest) {
-    return true;
+function findTownJudge(n, trust) {
+  const indegree = Array(n + 1).fill(0);
+  const outdegree = Array(n + 1).fill(0);
+
+  for (let i = 0; i < trust.length; i++) {
+    const [v1, v2] = trust[i];
+
+    indegree[v2]++;
+    outdegree[v1]++;
   }
-  visited.add(node);
 
-  for (const neighbour of graph[node]) {
-    if (!visited.has(neighbour)) {
-      if (isPathExists(graph, neighbour, dest, visited)) {
-        return true;
-      }
+  for (let i = 1; i <= n; i++) {
+    if (outdegree[i] === 0 && indegree[i] === n - 1) {
+      return i;
     }
   }
 
-  return false;
+  return -1;
 }
