908fcdc6cdff7aa26d362ee3c7a29ef5a2b7b70c
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-02-16 06:21:06 +0530

    graph dfs

--- script.js
+++ script.js
@@ -1,21 +1,10 @@
-function bfs(graph, srcNode) {
-  const visted = new Set();
-  const queue = [];
-  queue.push(srcNode);
-  visted.add(srcNode);
-  const result = [];
+function dfs(graph, node, visited, result) {
+  result.push(node);
+  visited.add(node);
 
-  while (queue.length) {
-    const curr = queue.shift();
-    result.push(curr);
-
-    for (const neighbour of graph[curr]) {
-      if (!visted.has(neighbour)) {
-        visted.add(neighbour);
-        queue.push(neighbour);
-      }
+  for (const neighbour of graph[node]) {
+    if (!visited.has(neighbour)) {
+      dfs(graph, neighbour, visited, result);
     }
   }
-
-  return result;
 }
