1174d6f880c34dfb50c6bea389fb0318b06119d0
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-02-17 07:25:25 +0530

    203. Remove Linked List Elements

--- script.js
+++ script.js
@@ -1,58 +1,38 @@
 /**
  * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
  * }
  */
-
 /**
- * @param {ListNode} headA
- * @param {ListNode} headBa
+ * @param {ListNode} head
+ * @param {number} val
  * @return {ListNode}
  */
-var getIntersectionNode = function (headA, headB) {
-  if (!headA || !headB) {
-    return null;
-  }
-  let curr1 = headA;
-  let curr2 = headB;
+var removeElements = function (head, val) {
+  let curr = head;
 
-  while (curr1 && curr2) {
-    curr1 = curr1.next;
-    curr2 = curr2.next;
+  while (curr && curr.val === val) {
+    curr = curr.next;
   }
 
-  if (curr1 === null) {
-    curr1 = headB;
-    while (curr2) {
-      curr1 = curr1.next;
-      curr2 = curr2.next;
-    }
-    curr2 = headA;
-
-    while (curr1 && curr2) {
-      if (curr1 === curr2) {
-        return curr1;
-      }
-      curr1 = curr1.next;
-      curr2 = curr2.next;
-    }
+  if (!curr) {
     return null;
   }
 
-  curr2 = headA;
-  while (curr1) {
-    curr2 = curr2.next;
-    curr1 = curr1.next;
-  }
-  curr1 = headB;
-  while (curr1 && curr2) {
-    if (curr1 === curr2) {
-      return curr1;
+  head = curr;
+
+  while (curr) {
+    let nextNode = curr.next;
+
+    while (nextNode && nextNode.val === val) {
+      nextNode = nextNode.next;
     }
-    curr1 = curr1.next;
-    curr2 = curr2.next;
+
+    curr.next = nextNode;
+    curr = nextNode;
   }
-  return null;
+
+  return head;
 };
