6f08ded3477d61aed95a71bd01b04f9d1493bb25
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-02-20 16:36:26 +0530

    Array.prototype.reduce

--- script.js
+++ script.js
@@ -1,28 +1,20 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} l1
- * @param {ListNode} l2
- * @return {ListNode}
- */
-var addTwoNumbers = function (l1, l2) {
-  let dummy = new ListNode();
-  let current = dummy;
-  let carry = 0;
-
-  while (l1 || l2 || carry) {
-    let sum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + carry;
-    carry = Math.floor(sum / 10);
-    current.next = new ListNode(sum % 10);
-    current = current.next;
-    l1 = l1 ? l1.next : null;
-    l2 = l2 ? l2.next : null;
+Array.prototype.myReduce = function (callback, initialValue) {
+  if (
+    typeof callback !== "function" ||
+    (initialValue === undefined && this.length === 0)
+  ) {
+    throw new TypeError("callback must be a function");
   }
 
-  return dummy.next;
+  let result = initialValue ? initialValue : this[0];
+  let startIndex = initialValue === undefined ? 1 : 0;
+
+  for (let i = startIndex; i < this.length; i++) {
+    if (isNaN(this[i])) {
+      continue;
+    }
+    result = callback(result, this[i], i, this);
+  }
+
+  return result;
 };
