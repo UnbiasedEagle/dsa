3316825f0e9e2819c2a9674417492122f39691e2
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-02-20 16:23:03 +0530

    2. Add Two Numbers

--- script.js
+++ script.js
@@ -6,26 +6,23 @@
  * }
  */
 /**
- * @param {ListNode} list1
- * @param {ListNode} list2
+ * @param {ListNode} l1
+ * @param {ListNode} l2
  * @return {ListNode}
  */
-var mergeTwoLists = function (list1, list2) {
+var addTwoNumbers = function (l1, l2) {
   let dummy = new ListNode();
   let current = dummy;
+  let carry = 0;
 
-  while (list1 && list2) {
-    if (list1.val < list2.val) {
-      current.next = list1;
-      list1 = list1.next;
-    } else {
-      current.next = list2;
-      list2 = list2.next;
-    }
+  while (l1 || l2 || carry) {
+    let sum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + carry;
+    carry = Math.floor(sum / 10);
+    current.next = new ListNode(sum % 10);
     current = current.next;
+    l1 = l1 ? l1.next : null;
+    l2 = l2 ? l2.next : null;
   }
-
-  current.next = list1 || list2;
 
   return dummy.next;
 };
