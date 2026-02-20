d609467916a1914a0afe5a74c8dcc223b203b51a
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-02-20 19:50:36 +0530

    Remove Node From End of Linked List

--- script.js
+++ script.js
@@ -11,48 +11,28 @@
 class Solution {
   /**
    * @param {ListNode} head
-   * @return {void}
+   * @param {number} n
+   * @return {ListNode}
    */
-  reorderList(head) {
-    const mid = this.getMid(head);
-    let head2 = mid.next;
-    mid.next = null;
-    head2 = this.reverseList(head2);
-    this.merge(head, head2);
-  }
-
-  merge(head1, head2) {
-    let curr1 = head1;
-    let curr2 = head2;
-    while (curr1 && curr2) {
-      const next1 = curr1.next;
-      const next2 = curr2.next;
-      curr1.next = curr2;
-      curr2.next = next1;
-      curr1 = next1;
-      curr2 = next2;
-    }
-  }
-
-  reverseList(head) {
-    let prev = null;
-    let curr = head;
-    while (curr) {
-      const next = curr.next;
-      curr.next = prev;
-      prev = curr;
-      curr = next;
-    }
-    return prev;
-  }
-
-  getMid(head) {
-    let slow = head;
+  removeNthFromEnd(head, n) {
     let fast = head;
-    while (fast && fast.next && fast.next.next) {
-      slow = slow.next;
-      fast = fast.next.next;
+    while (n > 0) {
+      fast = fast.next;
+      n--;
     }
-    return slow;
+
+    if (!fast) {
+      return head.next;
+    }
+
+    let slow = head;
+
+    while (fast.next) {
+      fast = fast.next;
+      slow = slow.next;
+    }
+
+    slow.next = slow.next.next;
+    return head;
   }
 }
