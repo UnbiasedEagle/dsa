b448c4d9e572eb71abe6e5ce89a4567b20f6f670
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-02-20 17:18:15 +0530

    Reorder Linked List

--- script.js
+++ script.js
@@ -1,31 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
 class Solution {
   /**
-   * @param {number[]} piles
-   * @param {number} h
-   * @return {number}
+   * @param {ListNode} head
+   * @return {void}
    */
-  minEatingSpeed(piles, h) {
-    let start = 1;
-    let end = Math.max(...piles);
-    let result = end;
-
-    while (start <= end) {
-      const mid = Math.floor((start + end) / 2);
-      if (this.canFinish(piles, mid, h)) {
-        result = mid;
-        end = mid - 1;
-      } else {
-        start = mid + 1;
-      }
-    }
-    return result;
+  reorderList(head) {
+    const mid = this.getMid(head);
+    let head2 = mid.next;
+    mid.next = null;
+    head2 = this.reverseList(head2);
+    this.merge(head, head2);
   }
 
-  canFinish(piles, k, h) {
-    let hours = 0;
-    for (let pile of piles) {
-      hours += Math.ceil(pile / k);
+  merge(head1, head2) {
+    let curr1 = head1;
+    let curr2 = head2;
+    while (curr1 && curr2) {
+      const next1 = curr1.next;
+      const next2 = curr2.next;
+      curr1.next = curr2;
+      curr2.next = next1;
+      curr1 = next1;
+      curr2 = next2;
     }
-    return hours <= h;
+  }
+
+  reverseList(head) {
+    let prev = null;
+    let curr = head;
+    while (curr) {
+      const next = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = next;
+    }
+    return prev;
+  }
+
+  getMid(head) {
+    let slow = head;
+    let fast = head;
+    while (fast && fast.next && fast.next.next) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+    return slow;
   }
 }
