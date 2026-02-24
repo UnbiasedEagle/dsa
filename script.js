2d9d66e604278b724fc0c3c4a7ab9f0b33719ef8
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-02-24 07:13:23 +0530

    225. Implement Stack using Queues

--- script.js
+++ script.js
@@ -1,21 +1,53 @@
-/**
- * @param {string[]} strs
- * @return {string[][]}
- */
-var groupAnagrams = function (strs) {
-  const map = new Map();
-
-  for (const str of strs) {
-    const freq = Array(26).fill(0);
-    for (const char of str) {
-      freq[char.charCodeAt(0) - "a".charCodeAt(0)]++;
-    }
-    const key = freq.join(",");
-    if (!map.has(key)) {
-      map.set(key, []);
-    }
-    map.get(key).push(str);
-  }
-
-  return Array.from(map.values());
+var MyStack = function () {
+  this.queue1 = [];
+  this.queue2 = [];
 };
+
+/**
+ * @param {number} x
+ * @return {void}
+ */
+MyStack.prototype.push = function (x) {
+  this.queue1.push(x);
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.pop = function () {
+  while (this.queue1.length > 1) {
+    this.queue2.push(this.queue1.shift());
+  }
+  const popped = this.queue1.shift();
+  [this.queue1, this.queue2] = [this.queue2, this.queue1];
+  return popped;
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.top = function () {
+  while (this.queue1.length > 1) {
+    this.queue2.push(this.queue1.shift());
+  }
+  const top = this.queue1[0];
+  this.queue2.push(this.queue1.shift());
+  [this.queue1, this.queue2] = [this.queue2, this.queue1];
+  return top;
+};
+
+/**
+ * @return {boolean}
+ */
+MyStack.prototype.empty = function () {
+  return this.queue1.length === 0;
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = new MyStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
