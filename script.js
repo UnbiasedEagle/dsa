6e37ff2d1fc4512a418919e9fd863a5a522a308a
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-02-24 08:58:15 +0530

    232. Implement Queue using Stacks

--- script.js
+++ script.js
@@ -1,18 +1,62 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
-var isValid = function (s) {
-  const stack = [];
-  const map = { "(": ")", "[": "]", "{": "}" };
+var MyQueue = function () {
+  this.stackIn = [];
+  this.stackOut = [];
+};
 
-  for (let char of s) {
-    if (char in map) {
-      stack.push(char);
-    } else if (stack.length === 0 || map[stack.pop()] !== char) {
-      return false;
-    }
+/**
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function (x) {
+  this.stackIn.push(x);
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function () {
+  while (this.stackIn.length > 0) {
+    this.stackOut.push(this.stackIn.pop());
   }
 
-  return stack.length === 0;
+  const result = this.stackOut.pop();
+
+  while (this.stackOut.length) {
+    this.stackIn.push(this.stackOut.pop());
+  }
+
+  return result;
 };
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function () {
+  while (this.stackIn.length > 0) {
+    this.stackOut.push(this.stackIn.pop());
+  }
+
+  const result = this.stackOut[this.stackOut.length - 1];
+
+  while (this.stackOut.length) {
+    this.stackIn.push(this.stackOut.pop());
+  }
+
+  return result;
+};
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function () {
+  return this.stackIn.length === 0 && this.stackOut.length === 0;
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
