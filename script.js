2c9373a23dfe5ad7e3b621a5d6f199319e4f4593
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-02-26 17:49:01 +0530

    739. Daily Temperatures

--- script.js
+++ script.js
@@ -1,27 +1,22 @@
 /**
- * @param {number[]} nums1
- * @param {number[]} nums2
+ * @param {number[]} temperatures
  * @return {number[]}
  */
-var nextGreaterElement = function (nums1, nums2) {
-  const result = Array(nums1.length).fill(-1);
-  const map = new Map();
-
-  for (let i = 0; i < nums1.length; i++) {
-    map.set(nums1[i], i);
-  }
-
+var dailyTemperatures = function (temperatures) {
+  const result = Array(temperatures.length).fill(0);
   const stack = [];
 
-  for (let i = nums2.length - 1; i >= 0; i--) {
-    while (stack.length > 0 && nums2[i] > stack[stack.length - 1]) {
+  for (let i = temperatures.length - 1; i >= 0; i--) {
+    while (
+      stack.length > 0 &&
+      temperatures[i] >= temperatures[stack[stack.length - 1]]
+    ) {
       stack.pop();
     }
     if (stack.length > 0) {
-      const index = map.get(nums2[i]);
-      result[index] = stack[stack.length - 1];
+      result[i] = stack[stack.length - 1] - i;
     }
-    stack.push(nums2[i]);
+    stack.push(i);
   }
 
   return result;
