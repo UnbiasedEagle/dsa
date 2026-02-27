cc5baabeb36d135b176e555895e6c626fe2f7db9
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-02-27 07:45:53 +0530

    69. Sqrt(x)

--- script.js
+++ script.js
@@ -1,62 +1,25 @@
 /**
- * @param {number[][]} grid
+ * @param {number} x
  * @return {number}
  */
-var orangesRotting = function (grid) {
-  let rottenIndex = [];
+var mySqrt = function (x) {
+  let left = 0;
+  let right = x;
+  let result = x;
 
-  let freshOranges = 0;
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    let square = mid * mid;
 
-  for (let i = 0; i < grid.length; i++) {
-    for (let j = 0; j < grid[i].length; j++) {
-      if (grid[i][j] === 1) {
-        freshOranges++;
-      }
-      if (grid[i][j] === 2) {
-        rottenIndex.push([i, j]);
-      }
+    if (square === x) {
+      return mid;
+    } else if (square < x) {
+      result = mid;
+      left = mid + 1;
+    } else {
+      right = mid - 1;
     }
   }
 
-  if (freshOranges === 0) return 0;
-
-  const queue = [];
-  let time = 0;
-
-  for (let i = 0; i < rottenIndex.length; i++) {
-    queue.push([rottenIndex[i][0], rottenIndex[i][1], 0]);
-  }
-
-  while (queue.length > 0) {
-    let [row, col, minutes] = queue.shift();
-    time = minutes;
-
-    const dirs = [
-      [-1, 0],
-      [1, 0],
-      [0, -1],
-      [0, 1],
-    ];
-
-    for (let [dx, dy] of dirs) {
-      let newRow = row + dx;
-      let newCol = col + dy;
-
-      if (
-        newRow >= 0 &&
-        newRow < grid.length &&
-        newCol >= 0 &&
-        newCol < grid[0].length &&
-        grid[newRow][newCol] === 1
-      ) {
-        grid[newRow][newCol] = 2;
-        freshOranges--;
-        queue.push([newRow, newCol, minutes + 1]);
-      }
-    }
-  }
-
-  if (freshOranges === 0) return time;
-
-  return -1;
+  return result;
 };
