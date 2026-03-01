91d1de3b6602400298aab16e5c229dabc0b164f6
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-03-01 09:01:41 +0530

    153. Find Minimum in Rotated Sorted Array

--- script.js
+++ script.js
@@ -2,39 +2,30 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findPeakElement = function (nums) {
-  if (nums.length === 1) {
-    return 0;
-  }
-
+var findMin = function (nums) {
   let left = 0;
   let right = nums.length - 1;
+  let min = Infinity;
 
   while (left <= right) {
-    let mid = Math.floor((left + right) / 2);
+    const mid = Math.floor((left + right) / 2);
 
-    if (mid === 0) {
-      if (nums[mid] > nums[mid + 1]) {
-        return mid;
+    if (nums[mid] < min) {
+      min = nums[mid];
+    }
+    // left part is sorted
+    if (nums[left] <= nums[mid]) {
+      if (nums[mid] <= nums[right]) {
+        right = mid - 1;
       } else {
         left = mid + 1;
       }
-    } else if (mid === nums.length - 1) {
-      if (nums[mid] > nums[mid - 1]) {
-        return mid;
-      } else {
-        right = mid - 1;
-      }
-    } else {
-      if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
-        return mid;
-      } else if (nums[mid] < nums[mid + 1]) {
-        left = mid + 1;
-      } else {
-        right = mid - 1;
-      }
+    }
+    // right part is sorted
+    else {
+      right = mid - 1;
     }
   }
 
-  return -1;
+  return min;
 };
