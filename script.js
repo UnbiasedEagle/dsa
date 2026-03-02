346caecbe45d2f161a718dd39ba417ede85f361f
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-03-02 08:27:46 +0530

    540. Single Element in a Sorted Array

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
 /**
- * @param {number[]} arr
+ * @param {number[]} nums
  * @return {number}
  */
-var peakIndexInMountainArray = function (arr) {
+var singleNonDuplicate = function (nums) {
+  if (nums.length === 1) return nums[0];
   let left = 0;
-  let right = arr.length - 1;
-
+  let right = nums.length - 1;
   while (right - left > 1) {
     const mid = Math.floor((left + right) / 2);
 
-    if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) {
-      return mid;
-    } else if (arr[mid] > arr[mid - 1] && arr[mid] < arr[mid + 1]) {
-      left = mid;
+    if (nums[mid] !== nums[mid + 1] && nums[mid] !== nums[mid - 1]) {
+      return nums[mid];
+    } else if (nums[mid] === nums[mid + 1]) {
+      if ((mid - left) % 2 === 0) {
+        left = mid + 2;
+      } else {
+        right = mid - 1;
+      }
     } else {
-      right = mid;
+      if ((right - mid) % 2 === 0) {
+        right = mid - 2;
+      } else {
+        left = mid + 1;
+      }
     }
   }
+
+  return nums[left];
 };
