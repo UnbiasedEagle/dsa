7b75057aa6a07d255fc94621a25128125dcc3303
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-03-02 08:43:49 +0530

    658. Find K Closest Elements

--- script.js
+++ script.js
@@ -1,30 +1,20 @@
 /**
- * @param {number[]} nums
- * @return {number}
+ * @param {number[]} arr
+ * @param {number} k
+ * @param {number} x
+ * @return {number[]}
  */
-var singleNonDuplicate = function (nums) {
-  if (nums.length === 1) return nums[0];
+var findClosestElements = function (arr, k, x) {
   let left = 0;
-  let right = nums.length - 1;
-  while (right - left > 1) {
-    const mid = Math.floor((left + right) / 2);
+  let right = arr.length - 1;
 
-    if (nums[mid] !== nums[mid + 1] && nums[mid] !== nums[mid - 1]) {
-      return nums[mid];
-    } else if (nums[mid] === nums[mid + 1]) {
-      if ((mid - left) % 2 === 0) {
-        left = mid + 2;
-      } else {
-        right = mid - 1;
-      }
+  while (right - left >= k) {
+    if (Math.abs(arr[left] - x) > Math.abs(arr[right] - x)) {
+      left++;
     } else {
-      if ((right - mid) % 2 === 0) {
-        right = mid - 2;
-      } else {
-        left = mid + 1;
-      }
+      right--;
     }
   }
 
-  return nums[left];
+  return arr.slice(left, right + 1);
 };
