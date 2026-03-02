01e7d1e6d476820f9166cb7c87cacc19abaf2c93
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-03-02 08:05:46 +0530

    852. Peak Index in a Mountain Array

--- script.js
+++ script.js
@@ -1,35 +1,20 @@
 /**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
+ * @param {number[]} arr
+ * @return {number}
  */
-var searchRange = function (nums, target) {
-  const first = findIndex(nums, target, true);
-  const last = findIndex(nums, target, false);
-  return [first, last];
-};
-
-function findIndex(nums, target, isFirst) {
-  let result = -1;
+var peakIndexInMountainArray = function (arr) {
   let left = 0;
-  let right = nums.length - 1;
+  let right = arr.length - 1;
 
-  while (left <= right) {
+  while (right - left > 1) {
     const mid = Math.floor((left + right) / 2);
 
-    if (nums[mid] === target) {
-      result = mid;
-      if (isFirst) {
-        right = mid - 1;
-      } else {
-        left = mid + 1;
-      }
-    } else if (nums[mid] < target) {
-      left = mid + 1;
+    if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) {
+      return mid;
+    } else if (arr[mid] > arr[mid - 1] && arr[mid] < arr[mid + 1]) {
+      left = mid;
     } else {
-      right = mid - 1;
+      right = mid;
     }
   }
-
-  return result;
-}
+};
