ce1c38a76f2caeb545bef87a0edf9beaf0f8930e
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-03-04 16:11:48 +0530

    28. Find the Index of the First Occurrence in a String

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
 /**
- * @param {string} s
- * @param {string} t
- * @return {boolean}
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
  */
-var isSubsequence = function (s, t) {
-  let sIndex = 0;
-  let tIndex = 0;
-
-  while (sIndex < s.length && tIndex < t.length) {
-    if (s[sIndex] === t[tIndex]) {
-      sIndex++;
-    }
-    tIndex++;
-  }
-
-  return sIndex === s.length;
+var strStr = function (haystack, needle) {
+  return haystack.indexOf(needle);
 };
