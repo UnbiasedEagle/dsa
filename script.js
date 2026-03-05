699a31948fbf07b3bdea6f2bc363e6471d385a19
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-03-05 07:28:24 +0530

    160. Intersection of Two Linked Lists

--- script.js
+++ script.js
@@ -1,8 +1,51 @@
 /**
- * @param {string} haystack
- * @param {string} needle
- * @return {number}
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
  */
-var strStr = function (haystack, needle) {
-  return haystack.indexOf(needle);
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function (headA, headB) {
+  let curr1 = headA;
+  let curr2 = headB;
+
+  while (curr1 && curr2) {
+    curr1 = curr1.next;
+    curr2 = curr2.next;
+  }
+
+  if (!curr1) {
+    curr1 = headB;
+    while (curr2) {
+      curr2 = curr2.next;
+      curr1 = curr1.next;
+    }
+    curr2 = headA;
+    while (curr1 && curr2) {
+      if (curr1 === curr2) return curr1;
+      curr1 = curr1.next;
+      curr2 = curr2.next;
+    }
+    return null;
+  }
+  if (!curr2) {
+    curr2 = headA;
+    while (curr1) {
+      curr1 = curr1.next;
+      curr2 = curr2.next;
+    }
+    curr1 = headB;
+    while (curr1 && curr2) {
+      if (curr1 === curr2) return curr1;
+      curr1 = curr1.next;
+      curr2 = curr2.next;
+    }
+    return null;
+  }
 };
