83f7a9a5943f7824bab616872a9965d044a87bae
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-03-05 16:42:02 +0530

    15. 3Sum

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
 /**
- * @param {number[]} height
- * @return {number}
+ * @param {number[]} nums
+ * @return {number[][]}
  */
-var maxArea = function (height) {
-  let maxWater = 0;
-
-  let left = 0;
-  let right = height.length - 1;
-  while (left < right) {
-    const water = (right - left) * Math.min(height[left], height[right]);
-    maxWater = Math.max(maxWater, water);
-    if (height[left] < height[right]) {
-      left++;
-    } else {
-      right--;
+var threeSum = function (nums) {
+  nums.sort((a, b) => a - b);
+  const result = [];
+  let i = 0;
+  while (i < nums.length - 2) {
+    if (i > 0 && nums[i] === nums[i - 1]) {
+      i++;
+      continue;
     }
+    let j = i + 1;
+    let k = nums.length - 1;
+    while (j < k) {
+      const sum = nums[i] + nums[j] + nums[k];
+      if (sum === 0) {
+        result.push([nums[i], nums[j], nums[k]]);
+        while (j < k && nums[j] === nums[j + 1]) j++;
+        while (j < k && nums[k] === nums[k - 1]) k--;
+        j++;
+        k--;
+      } else if (sum < 0) {
+        j++;
+      } else {
+        k--;
+      }
+    }
+    i++;
   }
-  return maxWater;
+  return result;
 };
