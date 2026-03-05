57a1f66f698d3770f6d4c356f120e09195c17706
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-03-05 09:56:29 +0530

    11. Container With Most Water

--- script.js
+++ script.js
@@ -1,51 +1,20 @@
 /**
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
+ * @param {number[]} height
+ * @return {number}
  */
+var maxArea = function (height) {
+  let maxWater = 0;
 
-/**
- * @param {ListNode} headA
- * @param {ListNode} headB
- * @return {ListNode}
- */
-var getIntersectionNode = function (headA, headB) {
-  let curr1 = headA;
-  let curr2 = headB;
-
-  while (curr1 && curr2) {
-    curr1 = curr1.next;
-    curr2 = curr2.next;
+  let left = 0;
+  let right = height.length - 1;
+  while (left < right) {
+    const water = (right - left) * Math.min(height[left], height[right]);
+    maxWater = Math.max(maxWater, water);
+    if (height[left] < height[right]) {
+      left++;
+    } else {
+      right--;
+    }
   }
-
-  if (!curr1) {
-    curr1 = headB;
-    while (curr2) {
-      curr2 = curr2.next;
-      curr1 = curr1.next;
-    }
-    curr2 = headA;
-    while (curr1 && curr2) {
-      if (curr1 === curr2) return curr1;
-      curr1 = curr1.next;
-      curr2 = curr2.next;
-    }
-    return null;
-  }
-  if (!curr2) {
-    curr2 = headA;
-    while (curr1) {
-      curr1 = curr1.next;
-      curr2 = curr2.next;
-    }
-    curr1 = headB;
-    while (curr1 && curr2) {
-      if (curr1 === curr2) return curr1;
-      curr1 = curr1.next;
-      curr2 = curr2.next;
-    }
-    return null;
-  }
+  return maxWater;
 };
