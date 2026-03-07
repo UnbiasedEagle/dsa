a887d84d5951beb40c0b2935d8cace8cecd86ee4
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-03-07 12:37:17 +0530

    424. Longest Repeating Character Replacement

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
 /**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
  */
-/**
- * @param {TreeNode} root
- * @return {number[]}
- */
-var inorderTraversal = function (root) {
-  const result = [];
-  function dfs(root) {
-    if (!root) return;
-    dfs(root.left);
-    result.push(root.val);
-    dfs(root.right);
+var characterReplacement = function (s, k) {
+  let left = 0;
+  let maxLength = 0;
+  const freqMap = Array(26).fill(0);
+  let maxFreq = 0;
+
+  for (let right = 0; right < s.length; right++) {
+    const char = s[right];
+    const charCode = char.charCodeAt(0) - "A".charCodeAt(0);
+    freqMap[charCode]++;
+    maxFreq = Math.max(maxFreq, freqMap[charCode]);
+
+    while (right - left + 1 - maxFreq > k) {
+      const leftChar = s[left];
+      const leftCharCode = leftChar.charCodeAt(0) - "A".charCodeAt(0);
+      freqMap[leftCharCode]--;
+      left++;
+    }
+
+    maxLength = Math.max(maxLength, right - left + 1);
   }
-  dfs(root);
-  return result;
+
+  return maxLength;
 };
