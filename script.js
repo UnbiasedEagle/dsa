b8177361cd4e2e0706bb0688d6fbe412573ca0a8
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-03-08 07:04:17 +0530

    102. Binary Tree Level Order Traversal

--- script.js
+++ script.js
@@ -8,16 +8,32 @@
  */
 /**
  * @param {TreeNode} root
- * @return {number[]}
+ * @return {number[][]}
  */
-var postorderTraversal = function (root) {
-  function dfs(root) {
-    if (!root) return;
-    dfs(root.left);
-    dfs(root.right);
-    result.push(root.val);
+var levelOrder = function (root) {
+  if (!root) {
+    return;
   }
   const result = [];
-  dfs(root);
+  const queue = [root, null];
+  let currentLevel = [];
+  while (queue.length > 0) {
+    const node = queue.shift();
+    if (node === null) {
+      result.push(currentLevel);
+      currentLevel = [];
+      if (queue.length > 0) {
+        queue.push(null);
+      }
+    } else {
+      currentLevel.push(node.val);
+      if (node.left) {
+        queue.push(node.left);
+      }
+      if (node.right) {
+        queue.push(node.right);
+      }
+    }
+  }
   return result;
 };
