c6fb5c188ad99b8bed03543e58314e004a1f42c5
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-03-12 21:57:16 +0530

    Power Of Two

--- script.js
+++ script.js
@@ -1,25 +1,5 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
+function isPowerOfTwo(n) {
+  return n > 0 && (n & (n - 1)) === 0;
+}
 
-/**
- * @param {TreeNode} root
- * @param {TreeNode} p
- * @param {TreeNode} q
- * @return {TreeNode}
- */
-var lowestCommonAncestor = function (root, p, q) {
-  if (!root) {
-    return null;
-  }
-  if (root.val < p.val && root.val < q.val) {
-    return lowestCommonAncestor(root.right, p, q);
-  } else if (root.val > p.val && root.val > q.val) {
-    return lowestCommonAncestor(root.left, p, q);
-  }
-  return root;
-};
+module.exports = { isPowerOfTwo };
