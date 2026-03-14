b0e8f2760a3019ed3e7ec9e8e58a5f0f91ea3547
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-03-14 16:41:05 +0530

    78. Subsets

--- script.js
+++ script.js
@@ -1,90 +1,24 @@
 /**
- * @param {number[][]} matrix
- * @param {number} k
- * @return {number}
+ * @param {number[]} nums
+ * @return {number[][]}
  */
-var kthSmallest = function (matrix, k) {
-  const maxHeap = new CustomMinHeap(k);
-  const rows = matrix.length;
-
-  for (let row = 0; row < rows; row++) {
-    maxHeap.add(matrix[row][0], 0, row);
-  }
-
-  while (k > 1) {
-    const [val, index, row] = maxHeap.pop();
-    if (index + 1 < matrix[row].length) {
-      maxHeap.add(matrix[row][index + 1], index + 1, row);
-    }
-    k--;
-  }
-
-  return maxHeap.pop()[0];
+var subsets = function (nums) {
+  const result = [];
+  backtrack(0, nums, result, []);
+  return result;
 };
 
-class CustomMinHeap {
-  constructor(k) {
-    this.k = k;
-    this.heap = [];
+function backtrack(index, nums, result, current) {
+  if (index === nums.length) {
+    result.push([...current]);
+    return;
   }
 
-  pop() {
-    if (this.heap.length === 0) return null;
-    if (this.heap.length === 1) return this.heap.pop();
-    const [val, index, row] = this.heap[0];
-    this.heap[0] = this.heap[this.heap.length - 1];
-    this.heap.pop();
-    this.heapifyDown();
-    return [val, index, row];
-  }
-
-  add(val, index, row) {
-    if (this.heap.length < this.k) {
-      this.heap.push([val, index, row]);
-      this.heapifyUp();
-    } else if (val < this.heap[0][0]) {
-      this.heap[0] = [val, index, row];
-      this.heapifyDown();
-    }
-  }
-
-  heapifyUp() {
-    let index = this.heap.length - 1;
-    while (index > 0) {
-      const parentIndex = Math.floor((index - 1) / 2);
-      if (this.heap[parentIndex][0] <= this.heap[index][0]) break;
-      [this.heap[parentIndex], this.heap[index]] = [
-        this.heap[index],
-        this.heap[parentIndex],
-      ];
-      index = parentIndex;
-    }
-  }
-
-  heapifyDown() {
-    let index = 0;
-    while (index < this.heap.length) {
-      const leftIndex = 2 * index + 1;
-      const rightIndex = 2 * index + 2;
-      let smallest = index;
-      if (
-        leftIndex < this.heap.length &&
-        this.heap[leftIndex][0] < this.heap[smallest][0]
-      ) {
-        smallest = leftIndex;
-      }
-      if (
-        rightIndex < this.heap.length &&
-        this.heap[rightIndex][0] < this.heap[smallest][0]
-      ) {
-        smallest = rightIndex;
-      }
-      if (smallest === index) break;
-      [this.heap[smallest], this.heap[index]] = [
-        this.heap[index],
-        this.heap[smallest],
-      ];
-      index = smallest;
-    }
-  }
+  current.push(nums[index]);
+  backtrack(index + 1, nums, result, current);
+  current.pop();
+  backtrack(index + 1, nums, result, current);
 }
+
+// Time Complexity: O(2^n)
+// Space Complexity: O(n)
