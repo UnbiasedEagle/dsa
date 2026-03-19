4faaccb1c6d45258395907ce34cf474e626a3e26
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-03-19 20:34:44 +0530

    455. Assign Cookies

--- script.js
+++ script.js
@@ -1,37 +1,18 @@
 /**
- * @param {number} n
- * @return {string[][]}
+ * @param {number[]} g
+ * @param {number[]} s
+ * @return {number}
  */
-var solveNQueens = function (n) {
-  const board = Array.from({ length: n }, () => Array(n).fill("."));
-  const result = [];
-  backtrack(board, result, 0, n);
-  return result;
-};
-
-function backtrack(board, result, row, n) {
-  if (row === n) {
-    result.push(board.map((row) => row.join("")));
-    return;
-  }
-  for (let col = 0; col < n; col++) {
-    if (isValid(board, row, col, n)) {
-      board[row][col] = "Q";
-      backtrack(board, result, row + 1, n);
-      board[row][col] = ".";
+var findContentChildren = function (g, s) {
+  g.sort((a, b) => a - b);
+  s.sort((a, b) => a - b);
+  let i = 0,
+    j = 0;
+  while (i < g.length && j < s.length) {
+    if (s[j] >= g[i]) {
+      i++;
     }
+    j++;
   }
-}
-
-function isValid(board, row, col, n) {
-  for (let i = 0; i < row; i++) {
-    if (board[i][col] === "Q") return false;
-  }
-  for (let i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
-    if (board[i][j] === "Q") return false;
-  }
-  for (let i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
-    if (board[i][j] === "Q") return false;
-  }
-  return true;
-}
+  return i;
+};
