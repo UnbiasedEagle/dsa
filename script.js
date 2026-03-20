99d8ef6bd855b203428ab67022a6eff03ef131b2
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-03-20 07:47:55 +0530

    57. Insert Interval

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 /**
- * @param {number[]} prices
- * @return {number}
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
  */
-var maxProfit = function (prices) {
-  let result = 0;
+var insert = function (intervals, newInterval) {
+  const result = [];
 
-  for (let i = 1; i < prices.length; i++) {
-    if (prices[i] > prices[i - 1]) {
-      result += prices[i] - prices[i - 1];
+  for (let i = 0; i < intervals.length; i++) {
+    let [start, end] = intervals[i];
+    if (end < newInterval[0]) {
+      result.push(intervals[i]);
+    } else if (start > newInterval[1]) {
+      result.push(newInterval);
+      newInterval = intervals[i];
+    } else {
+      newInterval[0] = Math.min(newInterval[0], start);
+      newInterval[1] = Math.max(newInterval[1], end);
     }
   }
-
+  result.push(newInterval);
   return result;
 };
