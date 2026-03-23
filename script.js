9836045243cccb50650d90270e3a9ae09dac718b
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-03-23 07:50:50 +0530

    1094. Car Pooling

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
 /**
- * @param {number[]} gas
- * @param {number[]} cost
- * @return {number}
+ * @param {number[][]} trips
+ * @param {number} capacity
+ * @return {boolean}
  */
-var canCompleteCircuit = function (gas, cost) {
-  const diff = gas.map((g, i) => g - cost[i]);
-  const total = diff.reduce((acc, curr) => acc + curr, 0);
-  if (total < 0) return -1;
-
-  let result = 0;
-  let totalGas = 0;
-  for (let i = 0; i < gas.length; i++) {
-    totalGas += diff[i];
-    if (totalGas < 0) {
-      result = i + 1;
-      totalGas = 0;
-    }
+var carPooling = function (trips, capacity) {
+  const timeLine = Array(1001).fill(0);
+  for (const [num, start, end] of trips) {
+    timeLine[start] += num;
+    timeLine[end] -= num;
   }
-  return result;
+  let current = 0;
+  for (const delta of timeLine) {
+    current += delta;
+    if (current > capacity) return false;
+  }
+  return true;
 };
