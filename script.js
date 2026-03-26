2fdfdea681659fd57f498a5437e3587af56a6c6e
Saurabh Singh <imsaurabhsingh007@gmail.com>
2026-03-26 09:55:44 +0530

    70. Climbing Stairs

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
  * @param {number} n
  * @return {number}
  */
-var fib = function (n) {
-  if (n <= 1) {
+var climbStairs = function (n) {
+  if (n <= 2) {
     return n;
   }
-  const dp = [0, 1];
-  for (let i = 2; i <= n; i++) {
+  const dp = [1, 2];
+  for (let i = 3; i <= n; i++) {
     dp[i] = dp[i - 1] + dp[i - 2];
   }
   return dp[n];
